755aeb5228da0b4f74f1fe64a1b8de61a1d33179
Gabe Jackson <gj@mail.co.de>
2017-09-25 20:21:04 -0400

    Fix typos; use const instead of let; use anonymous functions instead of arrow functions since they won't look quite so foreign to students looking at the test suite

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,50 +1,52 @@
 const expect = chai.expect;
 
-describe('drivers', () => {
-  describe('findMatching', () => {
-    it('returns all drivers that match the passed in name', () => {
-      let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+describe('index.js', function () {
+  describe('findMatching()', function () {
+    it('returns all drivers that match the passed in name', function () {
+      const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
       expect(findMatching(drivers, 'Bobby')).to.eql(['Bobby', 'Bobby']);
       expect(findMatching(drivers, 'Sammy')).to.eql(['Sammy']);
     });
 
-    it('returns matching drivers if case does not match but letters do', () => {
-      let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+    it('returns matching drivers if case does not match but letters do', function () {
+      const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
 
       expect(findMatching(drivers, 'Bobby')).to.eql(['Bobby', 'bobby']);
     });
 
-    it('returns an empty array if there is no match', () => {
-      let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+    it('returns an empty array if there is no match', function () {
+      const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
 
       expect(findMatching(drivers, 'Susan')).to.eql([]);
     });
   });
 
-  describe('fuzzyMatch', () => {
-    let driver;
+  describe('fuzzyMatch()', function () {
+    const drivers = [];
 
-    beforeEach(() => {
-      drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+    beforeEach(function () {
+      drivers.length = 0;
+
+      drivers.push('Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby');
     });
 
-    it('returns a driver if beginning provided letters match', () => {
+    it('returns a driver if beginning provided letters match', function () {
       expect(fuzzyMatch(drivers, 'Sa')).to.have.members(['Sammy', 'Sarah', 'Sally']);
     });
 
-    it('does not return drivers if only middle or ending letters match', () => {
+    it('does not return drivers if only middle or ending letters match', function () {
       expect(fuzzyMatch(drivers, 'y')).to.have.members([]);
     });
 
-    it('does not return drivers if only middle or ending letters match', () => {
+    it('does not return drivers if only middle or ending letters match', function () {
       expect(fuzzyMatch(drivers, 'mm')).to.have.members([]);
     });
   });
 
-  describe('matchName', () => {
-    it('accesses the data structure to check if name matches', () => {
-      let drivers = [
+  describe('matchName()', function () {
+    it('accesses the data structure to check if name matches', function () {
+      const drivers = [
         {
           name: 'Bobby',
           hometown: 'Pittsburgh' },
@@ -56,7 +58,7 @@ describe('drivers', () => {
           hometown: 'Cleveland' },
         {
           name: 'Annette',
-          hometown: 'Los Angelos' },
+          hometown: 'Los Angeles' },
         {
           name: 'Bobby',
           hometown: 'Tampa Bay' }
