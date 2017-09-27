bc821e473dcc1ee4e663da7f1ce2a7de11dab4f9
MargaritaKhodzka <mkhodzka@gmail.com>
2017-09-27 15:24:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching (collection, name) {
+  return collection.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase()
+  })
+}
+
+function fuzzyMatch (collection, beginningLetters) {
+  let nameLength = beginningLetters.length
+  return collection.filter(function(driverName) {
+    return driverName.slice(0, nameLength) === beginningLetters
+  })
+}
+
+function matchName (collection, name) {
+  return collection.filter(function(driver) {
+    return driver.name.toLowerCase() === name.toLowerCase()
+  })
+}
