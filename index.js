50d6584c8b3186ce048cadcf292b722268048b09
smithwebtek <brad@smithwebtek.com>
2017-10-02 19:14:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Code your solution in this file
+function findMatching(drivers, name){
+  let matches = []  
+  for (let driver of drivers){
+    if (match(driver, name)) {
+      matches.push(driver)
+    } else 
+    if (matchAnyCase(driver, name)){
+      matches.push(driver)
+    }
+  }
+  return matches
+}
+
+function match(name1, name2){
+  if (name1 === name2){
+    return name1
+  }
+}
+
+function matchAnyCase(name1, name2){
+  if (name1.toLowerCase() === name2.toLowerCase()){
+    return name1
+  }
+}
+
+function fuzzyMatch(drivers, string){
+  let matches = []
+  for (let driver of drivers){
+    if (matchString(driver, string)){
+      matches.push(driver)
+    }
+  }  
+  return matches
+}
+
+function matchString(driver, string){
+  if (driver.slice(0,string.length) === string){
+    return driver
+  }
+}
+
+function matchName(drivers, name){
+  let matches = []
+  for (const driver of drivers) {
+    if (match(driver.name, name)){
+      matches.push(driver)
+    }
+  }
+  return matches
+}
