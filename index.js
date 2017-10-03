b31f2126d12a3fc2872f388939ebf36cb63c2712
Tyler Gotto <tylergotto17@gmail.com>
2017-10-03 21:42:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+function findMatching(array, string) {
+   return array.filter(function (num) {
+     return num.toLowerCase() === string.toLowerCase();
+   });
+};
+function fuzzyMatch(array, string){
+  return array.filter(function (num) {
+    return num.slice(0, string.length) === string
+  });
+};
+function matchName(array, string){
+  return array.filter(function (num){
+    return num.name === string
+  });
+};
