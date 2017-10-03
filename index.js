77c2deb1cfdb0cf4dbc52c7a8900904c3a8aa52a
sukey <sdklos@gmail.com>
2017-10-03 09:11:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching(collection, name) {
+  return collection.filter(function (item) {
+    return item.toUpperCase() === name.toUpperCase();
+  })
+}
+
+function fuzzyMatch(collection, string) {
+  return collection.filter(function (item) {
+    return item.slice(0, string.length).toUpperCase() === string.toUpperCase();
+  })
+}
+
+function matchName(collection, string) {
+  return collection.filter(function (item) {
+    return item.name.toUpperCase() === string.toUpperCase();
+  })
+}
