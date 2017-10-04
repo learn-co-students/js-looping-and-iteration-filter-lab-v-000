4995a4d4aa2cb659a97ec7349e89e2bbd493f961
Hoon Lee <yl.hoony@gmail.com>
2017-10-04 16:44:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching(collection, name) {
+  return collection.filter(function(driver) {
+    return driver.toUpperCase() === name.toUpperCase();
+  });
+}
+
+function fuzzyMatch(collection, letters) {
+  return collection.filter(function(nameEach) {
+    return nameEach.slice(0, letters.length) === letters;
+  });
+}
+
+function matchName(collection, name) {
+  return collection.filter(function(driver) {
+    return driver.name === name;
+  })
+}
