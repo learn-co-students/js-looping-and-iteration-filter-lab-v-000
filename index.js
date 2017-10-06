6e579d9aaeefa91f7f010318ae837b9b8ed2e7fc
halfields <hfields697@aol.com>
2017-10-06 13:45:59 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file
+function findMatching(collection, name) {
+	const newCollection = [];
+	for (const member of collection) {
+		if (member.toLowerCase() === name.toLowerCase()) {
+			newCollection.push(member);
+		}
+	}
+	return newCollection;
+}
+
+
+function fuzzyMatch(collection, namePart) {
+	const newCollection = [];
+	for (const member of collection) {
+		if (member.startsWith(namePart)) {
+			newCollection.push(member);
+		}
+	}
+	return newCollection;
+}
+
+function matchName(collection, name) {
+	const newCollection = [];
+	for (const member of collection) {
+		if (member.name === name) {
+			newCollection.push(member);
+		}
+	}
+	return newCollection;
+} 
