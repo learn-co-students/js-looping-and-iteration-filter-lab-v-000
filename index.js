644b7278a45ac0c7ee5071c58bafaeae1e84b65b
github username pianistps <pianistps@yahoo.com>
2017-10-09 18:19:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  return drivers.filter(function(driver) { return driver.toLowerCase() === string.toLowerCase()})
+}
+
+function fuzzyMatch(drivers, string) {
+  let lengthOfString = string.length
+  return drivers.filter(function(driver) {
+    return driver.slice(0, lengthOfString) === string;
+  })
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function(driver) { return driver.name.toLowerCase() === string.toLowerCase()})
+}
