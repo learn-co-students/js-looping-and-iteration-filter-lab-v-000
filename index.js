1104368d38bf42692513e7eeb1a9b4487c0a7118
Jdville03 <jarrel.delottinville@gmail.com>
2017-10-10 11:03:01 -0400

    Done.

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function fuzzyMatch (drivers, name) {
 
 function matchName (drivers, name) {
   return drivers.filter(function (driver) {
-    return driver.name === name;
+    return driver.name.toLowerCase() === name.toLowerCase();
   });
 }
