f203d9a8f6beabf90a6ff68d74c93e53ff7155a9
CLackrone <charisaanne@gmail.com>
2017-10-10 12:10:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, letters) {
+  let lengthOfLetters = letters.length ;
+  return drivers.filter(function (driverName) {
+    return driverName.slice(0, lengthOfLetters) === letters;
+  });
+}
+
+function matchName(drivers, nameValue) {
+  return drivers.filter(function (driverName) {
+    return driverName['name'] === nameValue;
+  });
+}
