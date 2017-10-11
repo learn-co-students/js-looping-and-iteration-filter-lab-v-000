e7d1adca88d3c5718931c6ece5c345a20c23982d
Luisa Scavo <luisascavo@gmail.com>
2017-10-11 17:42:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+findMatching = (drivers, name) => {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+fuzzyMatch = (drivers, letters) => {
+  return drivers.filter(driver => driver.slice(0, letters.length) === letters)
+}
+
+matchName = (drivers, name) => {
+  return drivers.filter(driver => driver.name === name)
+}
