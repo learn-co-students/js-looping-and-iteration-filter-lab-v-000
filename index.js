464ca3a0b5f41792bfc8a82b412f288c3fe231c5
Jschles1 <jschles1@ramapo.edu>
2017-10-20 20:33:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  return drivers.filter((str) => { return str.toLowerCase() === string.toLowerCase() })
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter((str) => { return str.charAt(0) === string.charAt(0) })
+}
+
+function matchName(drivers, string) {
+  return drivers.filter((d) => { return d.name === string })
+}
