5a67f03a5452fe103ac40a84394e23b17ca57bbb
jimhotchkiss <jimbohtochkiss@yahoo.com>
2017-10-24 16:53:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(function(driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+ });
+}
