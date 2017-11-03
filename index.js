86166cc70eaffe7185705e83b461b31166a3d0ba
Rachel <rslim87@gmail.com>
2017-11-03 18:21:59 -0400

    Done.

--- index.js
+++ index.js
@@ -4,10 +4,31 @@ function findMatching(drivers, string) {
 	return drivers.filter(driver => driver === string || driver === string.toLowerCase())
 }
 
+// function findMatching (list, name) {
+//   return list.filter(function (driverName) {
+//     return driverName.toLowerCase() === name.toLowerCase();
+//   });
+// }
+
+
 function fuzzyMatch(drivers, string) {
 	return drivers.filter(driver => driver.charAt(0) === string.charAt(0))
 }
 
+// function fuzzyMatch (list, partialName) {
+//   let lengthOfName = partialName.length;
+//   return list.filter(function (driverName) {
+//     return driverName.slice(0, lengthOfName) === partialName;
+//   });
+// }
+
 function matchName(drivers, string) {
 	return drivers.filter(driver => driver.name === string)
 }
+
+
+// function matchName (list, name) {
+//   return list.filter(function (driver) {
+//     return driver.name.toLowerCase() === name.toLowerCase();
+//   });
+// }
