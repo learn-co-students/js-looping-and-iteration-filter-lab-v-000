7b66e0d32e85a506ed023d667f6b5b5100f69e1b
Ryan Do <falcsmike7@gmail.com>
2017-11-03 00:26:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching (collection, string) {
+  return collection.filter(function (element) { return element.toLowerCase() === string.toLowerCase(); });
+}
+
+function fuzzyMatch (collection, string) {
+  let user = string.length
+  return collection.filter(function (element) { return element.slice(0,user) === string; });
+}
+
+function matchName(collection, string) {
+  return collection.filter(function (element) { return element.name === string; });
+}
