8c6d4b145784de0fdc6273ba6759b1c103c094b0
Jake <jtb1137@gmail.com>
2017-11-07 16:35:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching (drivers, search) {
+	return drivers.filter(function (driver) {
+		return driver.toLowerCase() === search.toLowerCase();
+	});
+};
+
+function fuzzyMatch(drivers, search) {
+	return drivers.filter(function (driver) {
+		return driver.startsWith(search);
+	});
+};
+
+function matchName(drivers, search) {
+	return drivers.filter(function(driver) {
+		return driver.name == search;
+	});
+};
