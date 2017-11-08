93ba9f137a5ee5584b7833cec8b8c5bb14fbecfd
Ser Yoon Yun <sy1251@nyu.edu>
2017-11-08 05:51:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+function findMatching(drivers, string){
+  let arr = drivers.filter(function(driver){
+    return driver.toUpperCase() === string.toUpperCase();
+  });
+  return arr;
+}
+
+function fuzzyMatch(drivers, string){
+  let match = drivers.filter(function(driver){
+    return driver[0] === string[0];
+  });
+  return match;
+}
+
+function matchName(drivers, string){
+  let n = drivers.filter(function(driver){
+    return driver.name === string;
+  });
+  return n;
+}
