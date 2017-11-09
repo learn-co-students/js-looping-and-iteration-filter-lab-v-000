a37dcf88d277e6d3633a22c4c5e634fd87a32f6d
Jennifer Haddock <jentx123@hotmail.com>
2017-11-09 21:18:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(array, match){
+  return array.filter(function(matchIt){
+    return matchIt.toLowerCase() === match.toLowerCase();
+  })
+}
+
+function fuzzyMatch(array, match){
+  return array.filter(function(matchIt){
+    return matchIt[0] === match[0];
+  })
+}
+
+function matchName(array, name){
+  return array.filter(function(matchIt){
+    return matchIt.name === name
+  })
+}
