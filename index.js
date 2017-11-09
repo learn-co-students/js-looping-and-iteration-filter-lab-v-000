a8a938807243b8479a61b17c3cd9a661665cf056
veloblank <veloblank@gmail.com>
2017-11-09 14:54:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file
+function findMatching(drivers, string){
+  return drivers.filter(function(driver){
+    return driver.toLowerCase() === string.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, string){
+  return drivers.filter(function(driver){
+    return driver.slice(0, string.length) === string;
+  })
+}
+
+function matchName(drivers, string){
+  return drivers.filter(function(driver){
+    if (driver.name.toLowerCase() === string.toLowerCase()){
+      return driver.name;
+    } else {
+      
+    }
+  });
+
+}
