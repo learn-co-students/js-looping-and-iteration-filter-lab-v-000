5e179ff8363d4e7e573e03ff73905c03afdc6df6
kaisa <kaisa.piipari@gmail.com>
2017-11-10 22:48:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+
+function findMatching(collection, name) {
+    let names = collection.filter(
+        driver => driver.toLowerCase() === name.toLowerCase()
+    );
+    return names;
+}
+
+function fuzzyMatch(collection, name) {
+    let names = collection.filter(
+        driver => driver.substr(0, 2) === name.substr(0, 2)
+    );
+    return names;
+}
+
+function matchName(collection, name) {
+    let names = collection.filter(driver => driver.name === name);
+    return names;
+}
