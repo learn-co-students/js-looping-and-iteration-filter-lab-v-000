e18be95481a0a93dac9e889cbeb47723f402f57a
NickisKnowledge <lifechangingstuff@gmail.com>
2017-11-16 06:56:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(arr, name) {
+  return arr.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(arr, letters) {
+  return arr.filter(function (driverName) {
+    return driverName.slice(0, 2) === letters;
+  });
+}
+
+function matchName(arr, name) {
+  return arr.filter(function (driverArr) {
+    return driverArr.name === name;
+  });
+}
