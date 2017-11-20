44c2382fd401a98710f436b8f108dad4c757776c
burrahey <burra.archana@gmail.com>
2017-11-19 23:32:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(myArr, name) {
+   return myArr.filter(function (item){
+    return item.toUpperCase() === name.toUpperCase();
+  })
+}
+
+function fuzzyMatch(myArr, bit) {
+  return myArr.filter(function(item) {
+    return item.slice(0,2) === bit;
+  })
+}
+
+function matchName(myArr, given_name) {
+  return myArr.filter(function(obj) {
+    return obj["name"] === given_name;
+  })
+}
