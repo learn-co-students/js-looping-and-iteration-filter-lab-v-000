91b03037238f6065bcb6f0b8832acacbe6101482
Alexandra <github email address>
2017-11-22 08:44:42 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file
+function findMatching(drivers, target) {
+  let matches = []
+  target = target.toUpperCase()
+  for (let driver of drivers){
+    if (driver.toUpperCase() === target) {
+      matches.push(driver)
+    }
+  }
+  return matches
+}
+
+function wordMatch(word, query){
+  for (var index = 0; index < query.length; index++) {
+
+  }
+}
