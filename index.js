05eef135e2f4a0b59b3b640b5ab1308e4c60238e
Nicholas D'Amico <nickalan82@icloud.com>
2017-11-30 10:08:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Code your solution in this file
+
+///////////////////////////////////////////////////////
+// Helper Functions
+///////////////////////////////////////////////////////
+
+function filterName(driver, name) {
+	return driver.toLowerCase() === name.toLowerCase();
+}
+
+function letterMatch (name, letters) {
+	return name.slice(0, letters.length) === letters;
+}
+
+
+
+///////////////////////////////////////////////////////
+// Test Functions
+///////////////////////////////////////////////////////
+
+function findMatching (collection, name) {
+	const newCollection = [];
+	for (const driver of collection) {
+		if (filterName(driver,name)) {
+			newCollection.push(driver);
+		}
+	}
+	return newCollection;
+}
+
+function fuzzyMatch(collection, letters) {
+	const newCollection = [];
+	for (const driver of collection) {
+		if ( letterMatch(driver, letters) ) {
+			newCollection.push(driver);
+		}
+	}
+	return newCollection;
+}
+
+function matchName (collection, name) {
+	const newCollection = [];
+	for (let driver of collection) {
+		if ( filterName(driver.name, name) ) {
+			newCollection.push(Object.assign({}, driver));
+		}
+	}
+	return newCollection;
+}
