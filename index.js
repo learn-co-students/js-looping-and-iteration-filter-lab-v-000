93ac2030990586091a73dfb80838096168dbb353
marielfrank <marielbeth@gmail.com>
2017-12-03 17:25:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file
+function findMatching(array, string) {
+    return array.filter(function (driver) {
+        return driver.toLowerCase() === string.toLowerCase();
+    })
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(function (driver) {
+        return driver.slice(0, string.length) === string
+    })
+}
+
+function matchName(array, string) {
+    return array.filter(function (driver) {
+        return driver.name === string;
+    })
+}
