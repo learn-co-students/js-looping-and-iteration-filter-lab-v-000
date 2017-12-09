7721fc7404f5bbb1b89e2dd2b31881f5168c3e7e
Cynthia Rittenbach <crittenbach@gmail.com>
2017-12-09 20:30:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function findMatching(drivers, string) {
      return drivers.filter( function (driver) { return driver.toUpperCase() === string.toUpperCase(); });
-};
+}
 
 function fuzzyMatch(drivers, string) {
     return drivers.filter( function (driver) { return driver.slice(0,2).includes(string); });
-};
+}
 
 function matchName(drivers, string) {
     return drivers.filter( function (driver) { return driver.name === string; });
-};
+}
