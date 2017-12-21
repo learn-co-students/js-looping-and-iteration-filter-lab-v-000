d865829fc1826c0223d37d0c698c67a7a26e9e23
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-21 19:30:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file
+function findMatching(drivers, string){
+    return drivers.filter(function(i) {
+      return i.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function(i){
+    return i.slice(0, string.length) === string
+  })
+}
+
+function matchName(drivers, name) {
+  return drivers.filter( function(i){
+    return i.name === name
+  })
+}
