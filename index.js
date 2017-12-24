a5a3b1541775e4b6bb8576c06b1d0b2fcf4ebd59
Joey Cho <cjy12182000@naver.com>
2017-12-24 00:26:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching(drivers,str){
+  list = drivers.filter(function(element){return element.toUpperCase()===str.toUpperCase()})
+  return list
+}
+
+function fuzzyMatch(drivers,str){
+  return drivers.filter(function(element){return element.startsWith(str)})
+}
+
+function matchName(drivers,str){
+  return drivers.filter(function(element){return element.name === str})
+}
