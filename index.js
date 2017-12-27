e870c7a35c2d3f74543625856a9e312486cde9f2
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-12-27 15:36:28 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file
+function findMatching(array, string) {
+  return array.filter(function (driver) {
+    return driver.toLowerCase() === string.toLowerCase();
+  });
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter(function (driver) {
+    return driver.toLowerCase().slice(0, 2) === string.toLowerCase();
+  });
+}
+
+function matchName(array, string) {
+  return array.filter(function (driver) {
+    return driver.name.toLowerCase() === string.toLowerCase();
+  });
+}
