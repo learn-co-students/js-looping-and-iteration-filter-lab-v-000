b56e2a8199eef2bee2a8f7bb94ee74b227728003
Robin Pahlman <robin.pahlman@gmail.com>
2018-01-03 11:40:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  return drivers.filter(function(el) {
+    return el.toLowerCase() === string.toLowerCase(); 
+  })
+}
+
+function fuzzyMatch(drivers, string) {
+  let nameLength = string.length
+  return drivers.filter(function (el) {
+    return el.slice(0, nameLength) === string;
+  })
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function(el) {
+    return el.name === string; 
+  });
+}
