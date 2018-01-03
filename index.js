ada4c088f2718a02e6e5749de5c61e59a4cb2832
Adam Tropp <adt6261@gmail.com>
2018-01-03 06:26:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  return drivers.filter(function (name) { return string.toUpperCase() === name.toUpperCase(); });
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function (name) {return string[0] === name[0];});
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function (name) {
+    return name.name.toUpperCase() === string.toUpperCase();
+  });
+}
