a8e090112f6deeddf9e16aa2187f43a4b071b5cd
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2018-01-04 18:10:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  const collection = [];
+
+  for(const driver of drivers) {
+    if (driver.toUpperCase() === string.toUpperCase()) {
+      collection.push(driver)
+    }
+  }
+  return collection
+}
+
+function fuzzyMatch(drivers, string) {
+  const collection = [];
+
+  for(const driver of drivers) {
+    if (driver.startsWith(string)) {
+      collection.push(driver)
+    }
+  }
+  return collection
+}
+
+function matchName(drivers, string) {
+  const collection = [];
+
+  for(const driver of drivers) {
+    if (driver.name === string) {
+      collection.push(driver)
+    }
+  }
+  return collection
+}
