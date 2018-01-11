bdd8df28f955f772e62d23035be6663394bad359
dbfarms <matt.hunger@gmail.com>
2018-01-11 15:18:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+    const result = drivers.filter(function(driverName) { return (driverName.toLowerCase() === string.toLowerCase())})
+    
+    return result 
+}
+
+
+function fuzzyMatch(drivers, string){
+    const result = drivers.filter(function(driverName) { return (driverName[0].toLowerCase() == string[0].toLowerCase())})
+    return result 
+}
+
+function matchName(drivers, string){
+    const result = drivers.filter(function(driverName) { return (driverName.name.toLowerCase() == string.toLowerCase())})
+    return result 
+}
