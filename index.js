18ac21ce04acd43d670f2dac2cb4371c16aeb951
EC2 Default User <ec2-user@ip-10-0-0-95.ec2.internal>
2018-01-11 20:45:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers,str) {
+    return drivers.filter(function(e) {return e.toUpperCase() === str.toUpperCase()} );
+}
+
+function fuzzyMatch(drivers,str) {
+    return drivers.filter(function(e) {return e[0].toUpperCase() === str[0].toUpperCase()} );
+}
+
+function matchName(drivers,str) {
+    return drivers.filter(function(e) {return e.name.toUpperCase() === str.toUpperCase()} );
+}
