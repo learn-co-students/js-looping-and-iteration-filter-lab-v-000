2ad3b50e1afaec2dcfc7b57eb38cca9b08ab09f6
Lisa Lee <snickers495@gmail.com>
2018-01-13 03:40:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(word => word.toLowerCase() === name.toLowerCase());
+}
+function fuzzyMatch(drivers, beginning) {
+  return drivers.filter(word => word.slice(0, (beginning.length)) === beginning);
+}
+function matchName(drivers, match) {
+  return drivers.filter(arr => arr.name === match)
+}
