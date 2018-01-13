0055e5bd2712e825ac8a3fd67a44c5aeee8eb1fb
mandlerm <amaya.melissa@gmail.com>
2018-01-12 22:07:48 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  const names = drivers.filter(function(name) { return name.toUpperCase() === string.toUpperCase()})
+  return names
+}
+
+function fuzzyMatch(drivers, string) {
+  const driver = drivers.filter(function(name) {return name[0] === string[0]})
+  return driver
+}
+
+function matchName(drivers, string) {
+  const driver = drivers.filter(function(person) {return person.name === string})
+  return driver
+}
