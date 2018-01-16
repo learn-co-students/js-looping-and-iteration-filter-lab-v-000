358985580e0bfefb6c0a2e5fa422152036f2f82f
Elena Hopkins <elenamariehopkins@gmail.com>
2018-01-16 02:35:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(function (n) { return n.toUpperCase() === name.toUpperCase(); });
+}
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(function (l) { return l.toUpperCase().substring(0,letters.length) === letters.toUpperCase(); });
+}
+function matchName(drivers, name) {
+  return drivers.filter(function (n) { return n.name.toUpperCase() === name.toUpperCase(); });
+}
