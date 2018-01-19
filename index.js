db991b35f0df6e395544a9d8f3cebfac6ce3f2c2
alpbourne <alpbourne@gmail.com>
2018-01-18 21:38:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching (drivers, name) {
+  return drivers.filter(function(driversName) {
+    return driversName.toLowerCase() == name.toLowerCase();
+  });
+}
+
+function fuzzyMatch (drivers, letters) {
+  let firstLettersOfName = letters.length;
+  return drivers.filter(function(driversName) {
+    return driversName.slice(0, firstLettersOfName) === letters;
+  });
+}
+
+function matchName (drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
