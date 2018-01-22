e4523ef9b7e23e7d4a4d32068128344c18f636d6
lauraszyms <lauraszyms@gmail.com>
2018-01-21 18:02:37 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+ return drivers.filter(function (driverName) { return driverName.toUpperCase() === name.toUpperCase(); });
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(function (l) { return l.toUpperCase().substring(0,letters.length) === letters.toUpperCase(); })
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function (n) { return n.name.toUpperCase() === name.toUpperCase(); });
+}
