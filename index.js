0a2d3788232da51425c40ec7bbf958dafe1cf168
Enoch2k2 <enoch2k2@hotmail.com>
2018-01-26 09:53:45 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+    return drivers.filter((driver) => driver.toUpperCase() === name.toUpperCase());
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter((driver) => driver.substring(0, 2) == letters);
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
