e0ab5688de97bac16b6ebd162a2c77f3b99a9bbf
ackerm44 <ackerm44@gmail.com>
2018-01-29 17:50:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file
+function findMatching(collection, match) {
+  return collection.filter(function (name) {return match.toLowerCase() === name.toLowerCase() })
+}
+
+function fuzzyMatch(collection, match) {
+  return collection.filter(function (name) {return match === name.slice(0, match.length)})
+}
+
+function matchName(collection, match) {
+  return collection.filter(function (person) {return match === person['name']})
+}
