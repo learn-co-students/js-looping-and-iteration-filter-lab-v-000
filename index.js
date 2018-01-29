d154251a4adea80dc9235e281d3dace024e78168
github username <schun08@gmail.com>
2018-01-28 22:32:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(collection, name){
+  return collection.filter(function(person) {
+    return person.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(collection, name){
+  return collection.filter(function(person){
+    return person.slice(0, name.length) === name;
+  });
+}
+
+function matchName(collection, name){
+  return collection.filter(function(person) {
+    return person.name.toLowerCase() === name.toLowerCase();
+  });
+}
