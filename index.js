e0019cab1f451d87f2c178e40a93c12c79401481
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-02-07 01:58:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file
+function findMatching(drives, name) {
+    const newCollection = [];
+ 
+    for (const user of drives) {
+        if (user.toLowerCase()=== name.toLowerCase()) {
+          newCollection.push(user);
+        }
+    }
+     
+    return newCollection;
+}
+
+function fuzzyMatch(drives, string) {
+    const newCollection = [];
+    const l = string.length;
+    
+    for (const user of drives) {
+        if (user.slice(0,l)=== string) {
+          newCollection.push(user);
+        }
+    }
+    
+    return newCollection;
+    
+}
+
+function matchName(drives, name) {
+    const newCollection = [];
+   
+    for (const user of drives) {
+        if (user.name=== name) {
+          newCollection.push(user);
+        }
+    }
+    
+    return newCollection;
+    
+}
