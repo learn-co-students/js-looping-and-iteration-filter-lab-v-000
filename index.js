028b0c4c18d5acd6be2c12dd8749b813cd7c5862
EC2 Default User <ec2-user@ip-172-31-22-232.us-east-2.compute.internal>
2018-02-14 19:45:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+function findMatching (drivers, name) {
+   return drivers.filter(function (match) { 
+       return match.toLowerCase() === name.toLowerCase(); 
+       
+   });
+}
+
+function fuzzyMatch(drivers, name) {
+    let lengthName = name.length;
+    return drivers.filter(function (letter) {
+        return letter.slice(0, lengthName) === name;
+    });
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(function (names) {
+        return names.name === string;
+    });
+}
