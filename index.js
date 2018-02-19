870ba4eac97b05038002d21f9a404c73ef8b799f
Saad <Dropheaven@live.com>
2018-02-19 09:03:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file
+function findMatching(collection, string) {
+  return collection.filter(function (element) { return element.toLowerCase() === string.toLowerCase(); })
+}
+
+function fuzzyMatch(collection, letter) {
+  return collection.filter(function (element) { return element.startsWith(letter)})
+}
+
+function matchName(collection, name) {
+  return collection.filter(function (element) { return element.name === name })
+}
