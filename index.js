c98dcd55e01af3d5e276f4979bdb310cc9a4c31c
github username <github email address>
2018-02-20 11:38:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
 // Code your solution in this file
-function findMatching (drivers, name) {
+
+
+//findMatching- This function takes an array of drivers and a string as arguments, and returns
+//the matching list of drivers. The function should be case insensitive.
+function findMatching(drivers, name) {
   return drivers.filter(function (driverName) {
     return driverName.toLowerCase() === name.toLowerCase();
   });
 
-  //findMatching- This function takes an array of drivers and a string as arguments, and returns
-  //the matching list of drivers. The function should be case insensitive.
-}
+};
 
-function fuzzyMatch (drivers, nameString) {
-  let nameStringLength = nameString.length;
+function fuzzyMatch(drivers, substring) {
+  const substringLength = substring.length; // need the length of the substring so u kno how much to slice
   return drivers.filter(function (driverName) {
-    return driverName.slice(0, nameStringLength) === nameString;
+    return driverName.slice(0, substringLength) === substring;
   });
-
-  //This function takes an array of drivers and a string as arguments for querying the array, and returns
-  //all drivers whose names begin with the provided letters.
 }
 
-function matchName (drivers, name) {
-  return drivers.filter(function (driver) {
-    return driver.name.toLowerCase() === name.toLowerCase();
-  });
+//This function takes an array of drivers and a string as arguments for querying the array, and returns
+//all drivers whose names begin with the provided letters.
 
-  //This function takes an array of drivers and a string as arguments. In this function, each element of the
-  //drivers array is a JavaScript object that has a property of name. The function should return each element
-  //whose name property matches the provided string argument.
-}
+
+function matchName(drivers, name) {
+  return drivers.filter(function (driverObj) {
+    return driverObj.name.toLowerCase() === name.toLowerCase();
+  });
+};
+
+//This function takes an array of drivers and a string as arguments. In this function, each element of the
+//drivers array is a JavaScript object that has a property of name. The function should return each element
+//whose name property matches the provided string argument.
