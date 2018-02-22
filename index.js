b77935f0fb34748c4934b2a6a2dcff4113ee5156
Vicky Lau <vlau.nyc@gmail.com>
2018-02-22 07:00:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching (collection, nameToMatch) {
+  return collection.filter(function (driver) {
+    return driver.toLowerCase() === nameToMatch.toLowerCase();
+  });
+}
+
+function fuzzyMatch (collection, fuzzyName) {
+  return collection.filter(function (driver) {
+    return driver.slice(0, fuzzyName.length) === fuzzyName;
+  });
+}
+
+function matchName (collection, nameToMatch) {
+  return collection.filter(function (driver) {
+    return driver.name === nameToMatch;
+  });
+}
