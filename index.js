cac2e12dc10cede9d3f1a1c8776e0131a934cd90
github username <github email address>
2018-02-23 12:28:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching (array, name) {
+  return array.filter(function(driver) {
+     return driver.toLowerCase() === name.toLowerCase()
+   })
+}
+
+function fuzzyMatch (array, name) {
+  return array.filter(function(driver) {
+     return driver.toLowerCase()[0] === name.toLowerCase()[0]
+   })
+}
+
+function matchName (array, name) {
+  return array.filter(function(driver) {
+     return driver.name === name
+   })
+}
