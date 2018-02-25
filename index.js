024a8741b6e20a3d32dff44c08624e9c7aa347fe
Karen Berba <kberba@gmail.com>
2018-02-25 22:24:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file
+
+function findMatching(drivers, string) {
+  // let matchDriverList = [];
+  // for (const d of drivers) {
+  //   if (d.toUpperCase() === string.toUpperCase()) {
+  //     matchDriverList.push(d);
+  //   }
+  // }
+  // return matchDriverList;
+  return drivers.filter(driver => driver.toUpperCase() === string.toUpperCase());
+}
+
+function fuzzyMatch(drivers, string) {
+  // let matchDriverList = [];
+  // for (const d of drivers) {
+  //   if (d.substring(0, 2) === string.substring(0, 2)) {
+  //     matchDriverList.push(d);
+  //   }
+  // }
+  // return matchDriverList;
+  return drivers.filter(driver => driver.substring(0, 2) === string.substring(0, 2));
+}
+
+
+function matchName(drivers, string) {
+  return drivers.filter(driver => driver.name === string);
+}
