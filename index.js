30642d60cbae03ab9967afbf854716530dc666dd
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-03-02 01:48:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file
+function findMatching(array, name) {
+  const newarray = []
+  for (const element in array) {
+    if (array[element] == name) {
+      newarray.push(name)
+    } else if (array[element] == name.toLowerCase()) {
+      newarray.push(name.toLowerCase())
+    }
+  }
+  return newarray
+}
+
+function fuzzyMatch(array, letters) {
+  const newarray = []
+  for (const element in array) {
+    if (array[element].substring(0, letters.length) == letters) {
+    newarray.push(array[element])
+    }
+  }
+  return newarray
+}
+
+function matchName(object, name) {
+  const newobject = []
+  for (const element in object) {
+    if (object[element].name == name) {
+      newobject.push(object[element])
+    }
+  }
+  return newobject
+}
