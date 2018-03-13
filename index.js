a97ceca9a678e174ea5d8a78552a59cc442109ea
github username <github email address>
2018-03-13 09:57:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file
+// [1, 2, 3, 4, 5].filter(function (num) { return num > 3; });
+function findMatching(drivers, string){
+  return drivers.filter(function (driver) { return driver.toUpperCase() === string.toUpperCase(); })
+}
+
+function fuzzyMatch(drivers, string){
+  return drivers.filter(function (driver) { return driver.slice(0,1) === string.slice(0,1); })
+}
+
+function matchName(drivers, string){
+  return drivers.filter(function (driver) { return driver.name === string; })
+}
