5b36c959fe5e33cedb5ce891bb5c934823a623d3
Brittany Morris <brittmmendez@gmail.com>
2018-03-16 15:55:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file
+function findMatching(drivers,name){
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch (drivers, beginningLetters) {
+  return drivers.filter( function (driver){
+    return driver.substr(0, beginningLetters.length) == beginningLetters;
+  });
+}
+
+function matchName(drivers,name){
+  return drivers.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
