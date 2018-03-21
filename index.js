85e91e70609b6aa5aa1f9c7ef2db7d21d87d63a4
Erica <ericaplofsky@gmail.com>
2018-03-20 20:56:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file
+function findMatching(array, string) {
+  return array.filter(
+    function (name) {
+      if (name === string || name === string.toLowerCase()) {
+        return name;
+      }
+    });
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter(
+    function (name) {
+      return name.slice(0, string.length) === string;
+    }
+  );
+}
+
+function matchName(array, string) {
+  return array.filter(
+    function (user) {
+      return user.name === string;
+    }
+  );
+}
