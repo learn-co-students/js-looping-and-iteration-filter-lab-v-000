d5daa0d1df7ab82672d6a35a3155a8861c3c711a
David Towster <djt4xe@virginia.edu>
2018-04-02 21:29:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(function cb(driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  })
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(function cb(driver) {
+    return driver.slice(0,letters.length) === letters;
+  })
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function cb(driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  })
+}
+
