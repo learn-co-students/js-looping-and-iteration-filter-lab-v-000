d239b8b7ec0bbe102ab046b950dc476ece033697
blinded93 <blinded93@gmail.com>
2018-04-09 21:05:14 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file
+function findMatching (collection, name) {
+  const newCollection = collection.filter(function (user) { return user.toLowerCase() === name.toLowerCase() });
+  return newCollection;
+}
+
+function fuzzyMatch (collection, letters) {
+  const newCollection = collection.filter(function (user) { return user.substring(0, [letters.length]) === letters});
+  return newCollection;
+}
+
+function matchName (collection, name) {
+  const newCollection = collection.filter(function (user) { return user.name === name});
+  return newCollection;
+}
