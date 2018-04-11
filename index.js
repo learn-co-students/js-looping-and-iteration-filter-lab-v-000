a28586c29934e49415bb4d5c851de53c9a4b3495
independnt <gonzalezdennis6@gmail.com>
2018-04-11 11:05:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching(drivers, string){
+  const matching = drivers.filter(function(name){return name.toLowerCase() == string.toLowerCase()});
+  return matching;
+}
+function fuzzyMatch(drivers, string){
+  const matching = drivers.filter(function(letter){return letter.substring(0,2) === string.substring(0,2)});
+  return matching;
+}
+function matchName(drivers, person){
+  const matching = drivers.filter(function(match){return match.name === person});
+  return matching;
+}
