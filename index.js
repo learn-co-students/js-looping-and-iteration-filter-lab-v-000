6a2cce60a1a2c0415321a71414de02b66480b462
Garett Arrowood <garettarrowood@gmail.com>
2018-04-11 14:26:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+function findMatching(drivers, driver) {
+  return drivers.filter(function(name) {
+    return name.toUpperCase() === driver.toUpperCase();
+  })
+}
+
+function fuzzyMatch(drivers, letters) {
+  const numOfLetters = letters.length;
+
+  return drivers.filter(function(name) {
+    const nameFragment = name.slice(0, numOfLetters);
+    return nameFragment.toUpperCase() === letters.toUpperCase();
+  })
+}
+
+function matchName(drivers, driver) {
+  return drivers.filter(function(driverObj) {
+    return driverObj.name.toUpperCase() === driver.toUpperCase();
+  })
+}
