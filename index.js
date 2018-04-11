f1026881f32f98d711f9aa3ee17f48ae53ce0b96
blinded93 <blinded93@gmail.com>
2018-04-10 18:50:10 -0700

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 function findMatching (collection, name) {
-  const newCollection = collection.filter(function (user) { return user.toLowerCase() === name.toLowerCase() });
+  const newCollection = collection.filter(function (user) {
+    return user.toLowerCase() === name.toLowerCase();
+  });
   return newCollection;
 }
 
 function fuzzyMatch (collection, letters) {
-  const newCollection = collection.filter(function (user) { return user.substring(0, [letters.length]) === letters });
+  const newCollection = collection.filter(function (user) {
+    return user.substring(0, [letters.length]) === letters;
+  });
   return newCollection;
 }
 
 function matchName (collection, name) {
-  const newCollection = collection.filter(function (user) { return user.name === name });
+  const newCollection = collection.filter(function (user) {
+    return user.name === name;
+  });
   return newCollection;
 }
