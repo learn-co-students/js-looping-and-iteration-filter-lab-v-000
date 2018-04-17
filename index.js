73355d09a138f7d7f304797223b247a663fc4c43
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-04-17 14:49:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  return drivers.filter(function (driver) { return driver.toLowerCase() === string.toLowerCase(); });
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function (driver) { return driver.slice(0, driver.length-2).includes(string); });
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function (driverObject) { return driverObject.name === string; });
+}
+
+/*
+drivers = [
+  { name: value },
+  { name: value },
+  { name: value }
+]
+*/
