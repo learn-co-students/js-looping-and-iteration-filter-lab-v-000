a1879cdd25fae2f9125bf7862012fd2ddc7fa44d
catalanowebdevelopment <catalano.work@gmail.com>
2018-04-24 14:02:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+
+function findMatching(list, name) {
+    return list.filter(function (driverName) {
+       return driverName.toLowerCase() === name.toLowerCase()
+    })
+}
+
+function fuzzyMatch(array, partialName) {
+    return array.filter(function (name) {
+        return partialName === name.slice(0, partialName.length)
+    })
+}
+
+function matchName(collection, name) {
+    return collection.filter(function (collectionName) {
+        return collectionName.name === name
+    })    
+}
