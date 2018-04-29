5279cc26ba10d3f0969b32cf01d051d9847cfcd0
Obafemi Fadairo <ofpupp@princeton.edu>
2018-04-29 00:54:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  x = drivers.filter(function (driver_name) { if (driver_name.toUpperCase() == name.toUpperCase()) {return driver_name}; });
+  return x
+}
+
+function fuzzyMatch(drivers, string) {
+  length = string.length;
+  x = 3
+  x = drivers.filter(function (driver_name) { if (driver_name.startsWith(string)) { return driver_name[0][0]}; });
+  return x
+}
+
+function matchName(drivers, name) {
+  x = drivers.filter(function (driver) { if (driver.name == name) {return driver}; });
+  return x
+}
