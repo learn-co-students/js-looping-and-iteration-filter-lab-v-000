034b4fc2642fc03c28d57f0a6f780b332b6d7e4b
Jennifer Pazos <jmp521@gmail.com>
2018-05-02 22:47:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file
+findMatching = (array, string) =>  {
+  return array.filter(name => name.toLowerCase() === string.toLowerCase())
+
+}
+fuzzyMatch = (array, string) => {
+  let b = array.filter(function(name) {
+    if (name.split("")[0] === string.split("")[0]) {
+      return name
+    }
+
+  })
+
+  return b
+}
+
+
+matchName = (drivers, string) => {
+  return drivers.filter((name) => {
+    if(name.name === string) {
+      return string
+    }
+  })
+}
