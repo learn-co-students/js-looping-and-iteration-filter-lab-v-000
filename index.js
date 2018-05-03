b201fad620d78c238ec484a6a20925ffc10e5f7b
jacobsilver2 <jacobsilver2@mac.com>
2018-05-03 10:26:59 -0400

    done with step 1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file
+function findMatching(drivers, string) {
+    res = drivers.filter(function(e){return e === string; });
+    return res;
+}
+
+
+
+
+
+
+
+// 1. [1, 2, 3, 4, 5].filter(function (num) { return num > 3; });
+// 2. 4// => [4, 5]
+
+
+// The method accepts one argument, a callback function that it will invoke with each element in the array. For each element passed to the callback, if the callback's return value is truthy, that element is copied into a new array. If the callback's return value is falsy, the element is filtered out. After iterating over every element in the collection, .filter() returns the new array.
+// Now that you've built your own filter() method, you have a much deeper grasp of how JavaScript's built-in Array.prototype.filter() method works.
