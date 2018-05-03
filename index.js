387cea4af1846ff7c88ccd38e1ccf2aedcf531a9
jacobsilver2 <jacobsilver2@mac.com>
2018-05-03 10:33:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function findMatching(drivers, string) {
-    res = drivers.filter(function(e){return e === string; });
-    return res;
+    return drivers.filter(function(e){return e.toLowerCase() === string.toLowerCase(); });
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function(e){return e[0] === string[0]; });
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(function(e){return e.name === string; });
 }
 
 
@@ -15,3 +22,5 @@ function findMatching(drivers, string) {
 
 // The method accepts one argument, a callback function that it will invoke with each element in the array. For each element passed to the callback, if the callback's return value is truthy, that element is copied into a new array. If the callback's return value is falsy, the element is filtered out. After iterating over every element in the collection, .filter() returns the new array.
 // Now that you've built your own filter() method, you have a much deeper grasp of how JavaScript's built-in Array.prototype.filter() method works.
+
+  
