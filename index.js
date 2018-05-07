69aad7516748ddbadddf91e53d0c0373a88aac65
Man Leung <lepusleung@gmail.com>
2018-05-07 21:36:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(function(driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, initial) {
+  return drivers.filter(function(driverName) {
+    return initial === driverName.slice(0, initial.length);
+  });
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function(driver) {
+    return string === driver.name;
+  });
+}
