cb661f79154fab82e85d024e89eb600cd0d8acdd
Seb Gertz <sebgertz@gmail.com>
2018-05-09 22:23:30 +0000

    Refactored with native substring method

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
-  const newDrivers = drivers.filter(function (driver) { return driver.toLowerCase() === string.toLowerCase()})
-  return newDrivers;
+  return drivers.filter(function (driver) { return driver.toLowerCase() === string.toLowerCase()})
 }
 
 function fuzzyMatch(drivers, string) {
-  const newDrivers = drivers.filter(function (driver) {return driver[0].toLowerCase() === string[0].toLowerCase()})
-  return newDrivers;
+  const substringLength = string.length;
+  return drivers.filter(function (driver) {return driver.substring(0, substringLength).toLowerCase() === string.toLowerCase()})
 }
 
 function matchName(drivers, string) {
-  const newDrivers = drivers.filter(function(driver) {return driver.name === string})
-  return newDrivers;
+  return drivers.filter(function(driver) {return driver.name === string})
 };
