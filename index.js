0669c86834780d20924b81107dd3f70e4af28fd8
Randall Robinson <keith_adams007@yahoo.com>
2018-05-20 02:06:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(array, string) {
+ return array.filter(name => name.toUpperCase() === string.toUpperCase());
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter(name => name.startsWith(string))
+}
+
+function matchName(array, string) {
+  let result = []
+  for (const user of array) {
+     if (user.name === string) {
+       result.push(user)
+     }
+  } return result
+}
+
