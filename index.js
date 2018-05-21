32a98752958a2965db4f7c844f0e6c859d321a9a
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-05-21 12:10:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file
+function findMatching(drivers, findName) {
+  return drivers.filter(function (name) {return name.toLowerCase() === findName.toLowerCase();});
+}
+
+function fuzzyMatch(drivers, letters) {
+  const lettersLength = letters.length
+  return drivers.filter(function (name) {return name.slice(0, lettersLength) === letters; });
+}
+
+function matchName(drivers, findName) {
+  return drivers.filter(function (driver) {return driver.name === findName; });
+}
