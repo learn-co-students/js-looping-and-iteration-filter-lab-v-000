3b6747b39ccd04130022495bb7cd613f51bfcb69
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-05-22 17:13:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+
+
+function findMatching (drivers, name) {
+  return drivers.filter (function (x) {
+    return x === name.toLowerCase() || x === name });
+}
+
+function fuzzyMatch (drivers, name) {
+  return drivers.filter (function (x) {
+    return x.startsWith(name);
+  })
+}
+
+function matchName (drivers, str) {
+  return drivers.filter (function(x) {
+    return x.name === str;
+  })
+}
+
