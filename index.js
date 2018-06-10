f3fd95128a973ee3afff093a3eb438d61c080da2
Lisa Huang <lisa.yc.huang@gmail.com>
2018-06-09 21:21:00 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching(arr, str){
+    return arr.filter(driver => {
+        return driver.toUpperCase() === str.toUpperCase();
+    });
+};
+
+function fuzzyMatch(arr, str){
+    return arr.filter(driver => {
+        return driver.slice(0, str.length) === str;
+    })
+};
+
+function matchName(arr, str){
+    return arr.filter(driver => {
+        return driver.name === str;
+    })
+};
