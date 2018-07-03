a232e6e6798d407d9b1dc51e1bc287fb4c8d291f
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-07-03 01:55:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file
+
+
+function findMatching(drivers, string) {
+    const matches = []
+    for (const driver of drivers) {
+        if (driver.toLowerCase() === string.toLowerCase()) {
+            matches.push(driver)
+        }
+    }
+    return matches
+}
+
+
+function fuzzyMatch(drivers, partName) {
+    const partNameLength = partName.length;
+    return drivers.filter(function(driver) {
+        if (driver.slice(0, partNameLength) === partName) {
+            return driver
+        }
+    })
+}
+
+function matchName(drivers, matName) {
+    return drivers.filter(function(driverObj) {
+        for (const name in driverObj) {
+            if (matName === driverObj[name]) {
+                return matName
+            }
+        }
+    })
+}
