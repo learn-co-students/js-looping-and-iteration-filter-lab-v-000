6660a5267a4435185a3e5df777a6a08bc0d5a5fc
AspenJames <aspenjames@tqca.org>
2018-07-03 19:41:58 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  const matchingDrivers = [];
+  for(const driver of drivers) {
+    if (driver.toLowerCase() === string.toLowerCase()) {
+      matchingDrivers.push(driver);
+    }
+  }
+  return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, string) {
+  const matchingDrivers = [];
+  for(const driver of drivers) {
+    if(driver.slice(0, string.length) === string) {
+      matchingDrivers.push(driver);
+    }
+  }
+  return matchingDrivers;
+}
+
+function matchName(drivers, string) {
+  const matchingDrivers = [];
+  for(const driver of drivers) {
+    if (driver.name === string) {
+      matchingDrivers.push(driver);
+    }
+  }
+  return matchingDrivers;
+}
