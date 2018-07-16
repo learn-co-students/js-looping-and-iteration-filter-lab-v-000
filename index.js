099dac0ab9de31410034c3003cbba7910f051468
Marissa Zhong <mwz2101@columbia.edu>
2018-07-16 00:19:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+
+function findMatching(array, element) {
+  return array.filter(function (item) {return item.toLowerCase() === element.toLowerCase()});
+}
+
+function fuzzyMatch(array, element) {
+  return array.filter(function (item) {return item.slice(0,element.length) === element})
+}
+
+function matchName(array, element) {
+  return array.filter(function (item) {return item.name.toLowerCase() === element.toLowerCase()});
+}
