e6865416556f1794fc3610b759926e23b518672d
Rygel-XVI <micha232@umn.edu>
2018-07-17 05:40:17 -0700

    wip

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file
+function findMatching(array, thing) {
+  const newArray = [];
+  for (const item in array) {
+    if (array[item].toLowerCase() === thing.toLowerCase())
+    newArray.push(array[item]);
+  }
+  return newArray;
+}
+
+
+function fuzzyMatch(drivers, str) {
+  const newArray = [];
+  for (const item in array) {
+
+  }
+} 
