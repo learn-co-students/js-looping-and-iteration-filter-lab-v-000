347af08a8fa1ca46ae8a645a467900842720586c
MiriamPeskowitz <miriam5000@gmail.com>
2018-07-22 20:29:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']; 
+
+function findMatching(drivers, name) {
+  return drivers.filter(function (findName) {
+    return findName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+
+function fuzzyMatch(drivers, firstLetters) {
+  let nameLength = firstLetters.length;
+  return drivers.filter(function (findName) {
+   return findName.slice(0, nameLength) === firstLetters;
+    });
+}
+
+function matchName(drivers, name ) {
+  return drivers.filter(function(findName) {
+    return findName.name.toLowerCase() === name.toLowerCase();
+  });
+}
+
+
+
+
+
+
+// function matchName()
+
