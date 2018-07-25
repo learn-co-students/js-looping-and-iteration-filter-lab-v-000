a9240f2d46713b08c03cb8aba4473cb12f8eb319
AubreyNeimeier <neimeier.1@osu.edu>
2018-07-25 10:34:44 -0700

    two tests pass

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file
+function findMatching(collection,name){
+    return collection.filter(element => element.toUpperCase() === name.toUpperCase());
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(driver => driver.slice(0,2).toUpperCase() === string.toUpperCase())
+}
