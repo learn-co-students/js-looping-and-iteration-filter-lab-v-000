a3eed80392c9600a8a64f1f0434ee22a97f16c22
fonzmp <fonzmp@gmail.com[color]>
2018-07-26 11:50:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file
+function findMatching(array, name) {
+  search_holder = []
+  for (const person of array) {
+    if ( person.toUpperCase() === name.toUpperCase() ) {
+      search_holder.push(person);
+    }
+  } 
+  return search_holder;
+}
+
+function fuzzyMatch(array, name) {
+  search_holder = []
+  for (const person of array) {
+    if ( person.substring(0, name.length) == name ) {
+      search_holder.push(person);
+    }
+  }
+  return search_holder;
+}
+
+function matchName(array, name) {
+  search_holder = []
+  for (const person of array) {
+    if (person.name === name) {
+      search_holder.push(person);
+    }
+  }
+  return search_holder;
+}
