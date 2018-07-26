9aa17886fea819a8a251a6dbaa1ac13ec9a55ac6
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-07-25 20:53:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(driver, string) {
+  return driver.filter(word => word.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(driver, string) {
+  return driver.filter(word => word.slice(0, string.length) === string);
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(driver => driver["name"] === string);
+}
