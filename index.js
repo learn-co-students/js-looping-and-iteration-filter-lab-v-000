d49b665d1f00082af5f5005e95837acfe7960061
Naomi Dennis <njdennis94@gmail.com>
2018-07-27 14:54:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching(list, value){
+    return list.filter( (ele) => {return ele.toUpperCase() === value.toUpperCase()}  )
+}
+
+function fuzzyMatch(list, value){
+  return list.filter( (ele) => { return ele.slice(0, 2).toUpperCase()  === value.slice(0, 2).toUpperCase()  })
+}
+
+
+function matchName(list, value){
+  return list.filter( (ele) => {return ele['name'].toUpperCase() === value.toUpperCase() })
+}
