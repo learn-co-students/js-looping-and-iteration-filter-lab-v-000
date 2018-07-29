e8d22549b22253099c8984c976645a9139993978
Daniel Nunez <daniel.nunez.nyc@gmail.com>
2018-07-29 18:23:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file
+function findMatching(drivers, matcher) {
+  return drivers.filter(d => d.toLowerCase() === matcher.toLowerCase())
+}
+
+function fuzzyMatch(drivers, matcher) {
+  return drivers.filter(d => d.slice(0,2).includes(matcher))
+}
+
+function matchName(drivers, matcher) {
+  return drivers.filter(d => d.name === matcher)
+}
