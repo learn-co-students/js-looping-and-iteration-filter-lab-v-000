6da9253702b28376f65d1110a0a20a1fccf48c90
Diego Yanez <diegoyanez@MacBook-Pro-4.local>
2018-08-02 13:32:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+
+function findMatching(collection, name){
+  return collection.filter(function(driverNames){
+    return driverNames.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(collection, partOfName){
+  let nameLength = partOfName.length;
+  return collection.filter(function(driverName){
+    return driverName.slice(0, nameLength) === partOfName
+  })
+}
+
+function matchName(collection, name){
+  return collection.filter(
+    function(driver){
+      return driver.name.toLowerCase() === name.toLowerCase();
+    });
+}
