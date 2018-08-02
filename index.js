0857ab41d8a370800ad9301f8cf587cd40f4a0aa
abourke09 <aabourke@gmail.com>
2018-08-02 11:20:52 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(array, value) {
+  return array.filter(function(name) {
+    return name.toLowerCase() === value.toLowerCase();
+  });
+}
+
+function fuzzyMatch(array, value) {
+  return array.filter(function(name) {
+    return name[0] === value[0] && name[1] === value[1];
+  });
+}
+
+function matchName(nestedObject, value) {
+  return nestedObject.filter(function(person) {
+    return person.name === value;
+  });
+}
