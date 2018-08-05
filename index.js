5f8820608cf403d12f0538918016131f096daeb6
kanndide <kanndide@gmail.com>
2018-08-05 15:05:54 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file
+function findMatching(array, name) {
+
+	return array.filter(function (element) {return name.toLowerCase() == element.toLowerCase();});
+
+}
+
+function fuzzyMatch(array, letters) {
+	const reg = '^(' + letters + ')'
+	return array.filter(function (element) {return element.match(RegExp(reg))})
+}
+
+function matchName(array, name) {
+	return array.filter(function (driver) {return driver.name === name})
+}
