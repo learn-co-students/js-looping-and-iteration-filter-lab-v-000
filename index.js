46e460052d443539500831bb55c2f1c2b3210295
GabrielValle91 <gvalle91@gmail.com>
2018-08-05 18:31:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file
+function findMatching(aray, string){
+  const newAray = [];
+  for(const element of aray){
+    if (element.toUpperCase() === string.toUpperCase()){
+      newAray.push(element);
+    }
+  }
+  return newAray;
+}
+
+function fuzzyMatch(aray, string){
+  const newAray = [];
+  for(const element of aray){
+    if(element[0].toUpperCase() === string[0].toUpperCase()){
+      newAray.push(element);
+    }
+  }
+  return newAray;
+}
+
+function matchName(aray, string){
+  const newAray = [];
+  for(const element of aray){
+    if(element["name"] ===  string){
+      newAray.push(element);
+    }
+  }
+  return newAray;
+}
