11ed6fbe8b586970e8e26fc0d17a35ad2a032d53
mstmari <michael.landrum.st.marie@gmail.com>
2018-08-06 13:47:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file
+
+function findMatching(array, individualName) {
+return array.filter(function(name){return name.toUpperCase() === individualName.toUpperCase(); })
+}
+
+
+function fuzzyMatch(array, individualName) {
+return array.filter(function(name){return name.charAt() === individualName.charAt(); })
+}
+
+function matchName(object, individualName) {
+  return object.filter(function(name){return name.name === individualName; })
+
+}
