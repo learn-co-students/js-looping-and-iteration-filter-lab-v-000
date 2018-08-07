e6d01fdfaabee2d7c6a2aa5217e4dec570805814
Nicolas Travis <home@Nicolass-MacBook-Pro.local>
2018-08-07 17:13:01 +0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file
+function findMatching(collection, name){
+	let array = [];
+	for (const char of collection){
+		if (char.toLowerCase() === name.toLowerCase()){
+			array.push(char);
+		}
+	}
+	return array;
+};
+
+function fuzzyMatch(collection, term) {
+	let array = [];
+	for (const char of collection) {
+		if (char.includes(term) && term[0] === char[0]){
+			array.push(char);
+		}
+	}
+	return array;
+}
+
+function matchName(collection, term) {
+	let array = [];
+	for (const char of collection){
+		if (char.name === term){
+			array.push(char);
+		}
+	}
+	return array;
+};
