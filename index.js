604d834575ac21e0fec5910aa77af55b0ffc0be1
Branden Mosley <branden.mosley1337@gmail.com>
2018-08-12 21:14:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(drivers, query) {
+   return drivers.filter(function (name) {
+     return name.toLowerCase() === query.toLowerCase();
+   });
+}
+
+function fuzzyMatch(drivers, query) {
+  let queryLength = query.length;
+  return drivers.filter(function (name) {
+    return name.slice(0, queryLength) === query;
+  });
+}
+
+function matchName(drivers, query) {
+  return drivers.filter(function (driver) {
+    return driver.name.toLowerCase() === query.toLowerCase();
+  });
+}
