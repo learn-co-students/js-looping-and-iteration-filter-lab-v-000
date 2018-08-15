0596836e152d14f17dbfaf617d51ace457894e2d
learn <eric.desimone@cortland.edu>
2018-08-15 07:44:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+function findMatching (collection, name) {
+  return collection.filter(function(driver) {
+    return driver.toLowerCase() === name.toLowerCase()
+  })
+}
+
+function fuzzyMatch (collection, string) {
+  return collection.filter(function(driver) {
+    let endPoint = string.length;
+    let driverString = driver.slice(0, endPoint);
+    return driverString === string;
+  });
+}
+
+function matchName(collection, name) {
+  return collection.filter(function(driver) {
+    return driver.name === name;
+  })
+}
