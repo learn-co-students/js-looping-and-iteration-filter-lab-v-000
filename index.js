3a212daf080eead22cc9b68f44d4108daab15f58
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-20 19:27:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(array, name) {
+  const newarray = array.filter(word => word.toLowerCase() === name.toLowerCase());
+  return newarray;
+}
+
+function fuzzyMatch(array, string) {
+  function match(word) {
+    return word.slice(0, string.length).toLowerCase() === string.toLowerCase();
+  }
+  const newArray = array.filter(match);
+  return newArray;
+}
+
+function matchName(array, name) {
+  const newArray = array.filter(function (object) {return object['name'] === name})
+  return newArray;
+}
