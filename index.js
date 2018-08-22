f2332e4c39a6a98f870128876c1c62e77d2ccf74
Daniel Valmas <daniel.valmas@gmail.com>
2018-08-22 18:59:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+
+function findMatching(drivers, string) { 
+  return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase()); 
+} 
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(driver => driver.slice(0, string.length) === string);
+}
+
+// Test cases do not check if they are case sensitive... 
+function matchName(drivers, string) { 
+  return drivers.filter(driver => driver.name === string);
+}
