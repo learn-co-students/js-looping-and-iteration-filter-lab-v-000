463c9bda334571800033f9be99466bf9037f65fc
github username <github email address>
2018-08-24 16:17:05 -0400

    one problem left

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file
+
+function findMatching(array, name) {
+  newArray = array.filter(function(driverName) { return name.toLowerCase() == driverName.toLowerCase(); })
+  return newArray
+}
+
+function fuzzyMatch(array, name) {
+  newArray = array.filter(function(driverName) {return name[0] === driverName[0] && name[1] === driverName[1];})
+  return newArray
+
+}
+
+function matchName(array, driverName){
+newArray = array.filter(function(driverName) {return array.name == driverName;})
+return newArray
+}
