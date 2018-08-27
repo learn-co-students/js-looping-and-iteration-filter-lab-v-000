c9d7b5e6641bdfbabf6125c14e5b1cd0dd121284
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-27 23:41:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching (drivers, name){
+  name = name.toLowerCase();
+  return drivers.filter ( driver => driver.toLowerCase() === name );
+}
+
+function fuzzyMatch (drivers, str) {
+  return drivers.filter( driver => driver.startsWith(str) );
+}
+
+function matchName (drivers, name) {
+  return drivers.filter ( driver => driver.name === name );
+}
