0dd2436ac5d960d8c12269ec7ed7e4efb475a6db
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-27 19:25:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+
+function findMatching(drivers, name) {
+  const d = drivers.filter(function(driver) {return driver.toUpperCase() === name.toUpperCase()});
+  return d;
+}
+
+function fuzzyMatch(drivers, letters) {
+  const l = drivers.filter(function(driver) { return driver.startsWith(letters)});
+  return l;
+}
+
+function matchName(drivers, string) {
+  const d = drivers.filter(function(driver) {return driver.name === string})
+  return d
+}
