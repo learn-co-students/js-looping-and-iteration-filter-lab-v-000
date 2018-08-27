2d65cd7ebdb2a13da1e8839b9347122d8d216e91
Katrina Brinson <kbrinso@gmail.com>
2018-08-27 01:26:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching(data, query){
+  return data.filter(function (thing) { if (thing.toLowerCase() == query.toLowerCase()){return query;} });
+}
+
+function fuzzyMatch(data, part){
+  return data.filter(function(thing){if (part == thing.slice(0,part.length)){return thing;}});
+}
+
+
+function matchName(data, chosen){
+  return data.filter(function(val){if(val['name'] == chosen){return val;}});
+}
