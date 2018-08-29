8545a8044b3180feeb456ae8e75de4940979abb9
CSavi <carrielane89@gmail.com>
2018-08-29 11:11:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+function findMatching (collection, name) {
+    return collection.filter(function (driverName) {
+        return driverName.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(collection, letter) {
+   return collection.filter(function (driverName) { return driverName.slice(0, letter.length) === letter;
+    });
+}
+
+function matchName(collection, name) {
+    return collection.filter(function (driverName) { return driverName.name === name;
+    })
+}
