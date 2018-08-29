138dc3784d5bbe9b4ff070682502abc6b2b9cf53
Kkelly22 <kk504408@gmail.com>
2018-08-29 08:52:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(list, name) {
+	return list.filter(function (string) {return string.toUpperCase() === name.toUpperCase(); });
+}
+
+function fuzzyMatch(list, name) {
+	return list.filter(function (string) {return string.toUpperCase().substring(0,2) === name.toUpperCase().substring(0,2); });
+}
+
+function matchName (list, name) {
+	return list.filter(function (string) {debugger; return string.name.toUpperCase() === name.toUpperCase(); });
+}
