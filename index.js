bfc81402e53bd2e6aa645e1808efeda0405e1b0f
Jul Jen <juliajen.prog@gmail.com>
2018-09-04 23:44:32 +0000

    Done.

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ function findMatching(array, name) {
 
  function fuzzyMatch(array, name) {
    return array.filter(function (driver) {
-     return driver.startsWith(name) 
+     return driver.startsWith(name)
+   })
+ }
+
+ // takes an array of drivers and a string as arguments, each element of the drivers array is a JavaScript object that has a property of name. The function should return each element whose name property matches the provided string argument.
+
+ function matchName(collection, name) {
+   return collection.filter(function (driver) {
+     return driver.name === name
    })
  }
