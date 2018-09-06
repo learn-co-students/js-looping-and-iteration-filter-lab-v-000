f49d84edf6fb02ed17f9cdeb80d7b74bf89631f9
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-06 19:54:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+function findMatching(drivers,string) {
+const match =  drivers.filter(function (item) {return item.toUpperCase() === string.toUpperCase() })
+  return match
+}
+
+function fuzzyMatch(drivers, string) {
+
+  const newArray =  drivers.filter(function (item) {return item[0] === string[0] && item[1] === string[1]})
+    return newArray
+}
+
+function matchName(drivers,string) {
+  const newArray =  drivers.filter(function (item) {return item.name === string})
+    return newArray
+}
