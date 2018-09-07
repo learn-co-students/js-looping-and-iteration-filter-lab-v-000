ace65102df3b9243d19d6086b0af0ec70ba735fe
Veronica Bustabad <vbustabad@gmail.com>
2018-09-07 02:14:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+
+function findMatching(drivers, name) {
+  const new_drivers_array = drivers.filter(function (driver) { return driver.toLowerCase() === name.toLowerCase(); });
+  return new_drivers_array;
+}
+
+function fuzzyMatch(drivers, letters) {
+  const new_drivers_array = drivers.filter(function (driver) { return driver.slice(0, 2) === letters; });
+  return new_drivers_array;
+}
+
+function matchName(drivers, name) {
+  const new_drivers_array = drivers.filter(function (driver) { return driver.name === name; });
+  return new_drivers_array;
+}
