dc43c6bfc20b12f698c2bf571fd24d48d1cfca2b
Petya Djanovska <petya.djanovska@gmail.com>
2018-09-17 20:32:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(array, sample) {
+  return array.filter(name => name.toLowerCase() === sample.toLowerCase());
+}
+
+function fuzzyMatch(array, sample) {
+  return array.filter(name => name.substring(0,sample.length) === sample);
+}
+
+function matchName(array, sample) {
+  const newCollection = [];
+
+  for (const user of array) {
+    if (user.name === sample) {
+      newCollection.push(user);
+    }
+  }
+  return newCollection;
+}
