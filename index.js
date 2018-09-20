a47ae86b953c64496a12e2d69eb9632253f036d6
swgoodman <spencerwgoodman@icloud.com>
2018-09-20 12:25:01 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+
+function findMatching(list, string) {
+  return list.filter( function(driverName) {
+    return driverName.toLowerCase() === string.toLowerCase();
+  });
+}
+
+function fuzzyMatch(list, partial) {
+  let partialLength = partial.length;
+
+  return list.filter( function(driverName) {
+    return driverName.slice(0, partialLength) === partial;
+  });
+}
+
+function matchName(list, string) {
+  return list.filter( function(driverName) {
+    return driverName.name.toLowerCase() === string.toLowerCase();
+  });
+}
