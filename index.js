7c0120b0bebacf72ffbd84506db0077063046669
hkleindl <hkleindl@gmail.com>
2018-09-26 12:01:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(function(el) { return el.toLowerCase() === name.toLowerCase();});
+}
+
+function fuzzyMatch(drivers, str) {
+  return drivers.filter(function(el) {return el.startsWith(str);});
+}
+
+function matchName(drivers, str) {
+  return drivers.filter(function(obj) {return obj['name'] === str;});
+}
