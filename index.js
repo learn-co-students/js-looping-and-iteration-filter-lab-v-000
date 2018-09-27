118fdffb16760241cc2fd79087c5ecf2e42f4c8d
amckean12 <amckean@me.com>
2018-09-27 17:08:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file
+
+function findMatching(array, name)
+{
+  return array.filter(function (driverName)
+  {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+
+function fuzzyMatch(array, name)
+{
+  let lengthOfName = name.length;
+  return array.filter(function(driverName)
+  {
+    return driverName.slice(0, lengthOfName) === name;
+  }
+);
+}
+
+function matchName(array, name)
+{
+  return array.filter(function(driver)
+  {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  }
+);
+}
