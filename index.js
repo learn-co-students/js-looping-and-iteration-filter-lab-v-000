5839adeda75476637ba6b6d2f5970e5debd0ab3a
Nate Miller <natekmiller96@gmail.com>
2018-09-28 15:13:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file
+function findMatching(drivers, string){
+  
+const result = drivers.filter(word => word.toLowerCase() === string.toLowerCase());
+
+return result;
+}
+
+
+function fuzzyMatch(drivers, string){
+  function fuzzy(word, string){
+    const slength = string.length;
+    const slicedWord = word.slice(0, slength)
+    if (slicedWord === string){
+      return word
+    }
+  }
+
+  const result = drivers.filter(word => fuzzy(word, string));
+  return result;
+}
+
+function matchName(drivers, string){
+  const result = drivers.filter(word => word.name === string);
+  return result;
+}
