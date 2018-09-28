7ea009b1f12bb0719da203dd7499565ca37922a7
stazman <cadistasio@gmail.com>
2018-09-28 16:51:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching (collection, driver) {
+    return collection.filter(function (driverName) { 
+        return driverName.toLowerCase() === driver.toLowerCase();
+    });
+} 
+    
+function fuzzyMatch (collection, driver)
+
+return drvName if the first two letters in the driver matchMedia
+
+if drvName.slice(0, 1)
+
+
+// for (const char of 'Hello, world!') {
+//     console.log(char);
+// }
+ 
+// // LOG: H
+// // LOG: e
+// // LOG: l
+// // LOG: l
+// // LOG: o
+// // LOG: ,
+// // LOG:
+// // LOG: w
+// // LOG: o
+// // LOG: r
+// // LOG: l
+// // LOG: d
+// // LOG: !
+
+// https://learn.co/tracks/full-stack-web-development-v6/javascript/looping-and-iteration/object-iteration
+
+
+
