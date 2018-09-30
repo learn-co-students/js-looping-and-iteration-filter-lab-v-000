cd015344d8fe55f2de6567ee720783dd1a2adf65
Jayon Kim <jpkim921@gmail.com>
2018-09-30 19:56:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching(arr, str) {
+  return arr.filter(function (driver) {
+    return driver.toLowerCase() === str.toLowerCase();
+  });
+}
+
+function fuzzyMatch(arr, str) {
+  return arr.filter(function (driver) {
+    return driver.slice(0,2) === str
+  });
+}
+
+function matchName(arr, str) {
+  return arr.filter(function (driver) {
+    return driver.name === str;
+  });
+}
