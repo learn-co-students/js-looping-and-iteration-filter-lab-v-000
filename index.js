448fae9783c8b0680514dad0027402b8e1669157
stazman <cadistasio@gmail.com>
2018-10-01 15:24:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Code your solution in this file
-// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
-
 function findMatching (collection, driver) {
     return collection.filter(function (driverName) { 
         return driverName.toLowerCase() === driver.toLowerCase();
@@ -12,10 +9,6 @@ function fuzzyMatch (collection, driver) {
         return driverName.slice(0, 1) === driver.slice(0, 1); 
     });
 }
-
-// So fuzzyMatch filters the collection to see how the argument passed through the filter 
-// compares with the argument passed through fuzzyMatch (presumably referring to an existing declared object)
-//driverName is kind of a proxy for the driver
 
 function matchName (collection, driver) {
     return collection.filter(function (driverName) {
