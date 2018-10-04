9d9dc69a334368266708a644e0b7aea3858f54b6
Kevin Huang <yellow.khaos@gmail.com>
2018-10-04 22:12:43 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+function findMatching(collection, name) {
+
+  let result = collection.filter(function(individual) {return name.toUpperCase()===individual.toUpperCase()})
+  return result
+}
+
+function fuzzyMatch(collection, search) {
+  let result = collection.filter(function(individual) {return search === individual.slice(0,search.length)})
+  return result
+}
+
+function matchName(collection, name) {
+  let result = collection.filter(function(individual) {return name === individual.name})
+  return result
+}
