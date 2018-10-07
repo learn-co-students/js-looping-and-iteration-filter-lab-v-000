95cc270bd7b099eb068e419c75301c372922154a
Daniel Hernandez <elreymcd@yahoo.com>
2018-10-06 21:25:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file
+// Code your solution in this fil
+function findMatching(collection, name){
+    let matchedDrivers = [];
+    for (const element of collection){
+        if (name.toUpperCase() === element.toUpperCase()) {
+        matchedDrivers.push(element);
+        }
+    }
+    return matchedDrivers;
+}
+
+function fuzzyMatch(collection, name){
+    let matchedDrivers = [];
+    nameLength = name.length;
+    for (const element of collection){
+        if (element.startsWith(name)){
+            matchedDrivers.push(element);
+        }
+    }
+    return matchedDrivers;
+}
+
+function matchName(collection, thename){
+    matchedDrivers = [];
+    for (const driver of collection){
+        if (driver.name === thename){
+            matchedDrivers.push({name: driver.name, hometown: driver.hometown});
+        }
+    }
+    return matchedDrivers;
+}
+
