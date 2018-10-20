1c860076f34809d675c2bd5331374604d70bc572
eyi1 <ej7729@gmail.com>
2018-10-20 00:12:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching(collection, name){
+    return collection.filter(function(driver){
+        return driver.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(list, partialName){
+    let nameLength = partialName.length
+    return list.filter(function(driver){
+        return driver.slice(0, nameLength) === partialName;
+    });
+}
+
+function matchName(collection, name){
+    return collection.filter(function(driver){
+        return driver.name.toLowerCase() === name.toLowerCase();
+    });
+}
