3b3d6ce587667416d612ca0750596c76c0a4f42b
Carol Greene <carolgreene3@gmail.com>
2018-10-21 00:02:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-// Code your solution in this file
-function findMatching(drivers, name) {
-  return drivers.filter(function (driverName) { return driverName.toUpperCase()=== name.toUpperCase();});
+
+function findMatching(drivers, name) {                  //This is a string, not an object w/properties.
+  return drivers.filter(function (driverName) {         //return the drivers array & filter it to return individual string.(call the string driverName)
+    return driverName.toUpperCase() === name.toUpperCase()    //return the driverName string to upper case that equals the name argument
+  })                                                         //to upper case
 }
 
-  function fuzzyMatch(drivers, letters) {
-    return drivers.filter(driver => driver.startsWith(letters) );
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(driver => driver.startsWith(string))     //use the .startsWith()
+}
 
-  }
+//or
+//function fuzzyMatch(drivers, string) {             //drivers array & string
+//  let lengthOfName = string.length;               //set variable to the number of the length of the string
+//  return drivers.filter(function (driverName) {     //return drivers array & filter it to return individual driver. set to driverName.
+//    return driverName.slice(0, lengthOfName) === string   //return driverName & slice it to return letters starting a 0 index &
+//  })                                                      //lasting for the lengthOfName variable
+//}
 
-  function matchName(drivers, name) {
-    return drivers.filter(driver => driver.name === name);
-  }
+function matchName(drivers, name) {                      //This is an object with a name property
+  return drivers.filter(driver => driver.name === name)   //return drivers array & filter it to return individual driver whose
+}                                                         //name property === the name passed in
