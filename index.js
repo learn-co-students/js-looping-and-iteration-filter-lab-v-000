abfa34452de0269658a4affbc0f0f6d6b3f81254
zep1994 <snaxbags@protonmail.com>
2018-10-27 15:15:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  return drivers.filter(function (driverName) {
+    return driverName.toUpperCase() === string.toUpperCase();
+  });
+};
+
+function fuzzyMatch(drivers, string) {
+  let nameChar = string.length;
+
+  return drivers.filter(function (driverName) {
+    return driverName.slice(0, nameChar) === string;
+  });
+};
+
+function matchName(drivers, string) {
+  return drivers.filter(function (driverName) {
+    return driverName.name.toUpperCase() === string.toUpperCase();
+  });
+};
