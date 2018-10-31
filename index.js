c43056fa4916b4864c1ac1920db0ead2e4fcadbd
anikonchuk <anikonch@gmail.com>
2018-10-31 12:44:50 -0400

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function fuzzyMatch (drivers, string) {
 }
 
 function matchName (drivers, string) {
-  return drivers.filter(function (driver) {return driver.name === string;});
+  return drivers.filter(function (driver) {return driver.name.toUpperCase() === string.toUpperCase();});
 }
