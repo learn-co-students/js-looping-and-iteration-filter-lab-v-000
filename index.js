3f7e3b3f1cab518f344418ea48b34218d5ce4196
monksandninjas <blackbeltdiaries@gmail.com>
2018-11-09 11:18:47 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter( function (driverName)
+    { return driverName.toLowerCase() === name.toLowerCase() })
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter( function (driverName)
+    { return driverName.slice(0, name.length).toLowerCase() === name.toLowerCase() })
+}
+
+function matchName(drivers, name) {
+  return drivers.filter( function (driverName)
+    { return driverName.name.toLowerCase() === name.toLowerCase() })
+}
