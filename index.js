caf7388f98299c985c032c86029a6f12f9fffd58
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-09 08:04:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file
+function findMatching(drivers, string) {
+  const matches = drivers.filter(function(driver) {return driver.toLowerCase() === string.toLowerCase()})
+  return matches
+}
+
+function fuzzyMatch(drivers, string) {
+  const matches = drivers.filter(function(driver) {return driver[0].toLowerCase() === string[0].toLowerCase()})
+  return matches
+}
+
+function matchName(drivers, string) {
+  const matches = drivers.filter(function(driver) {return driver.name.toLowerCase() === string.toLowerCase()})
+  return matches
+}
