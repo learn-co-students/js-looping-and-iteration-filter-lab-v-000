3fe982993f2d553c53762574a8a6e48859543835
github username <github email address>
2018-11-12 10:02:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file
+
+function findMatching(drivers, firstName) {
+   return drivers.filter(function (individualDriver) {
+    return individualDriver.toUpperCase() === firstName.toUpperCase();
+   });
+}
+
+function fuzzyMatch(drivers, firstLetters) {
+    return drivers.filter(function (individualDriver) {
+        return individualDriver.slice(0, firstLetters.length) === firstLetters;
+    });
+}
+
+function matchName(drivers, firstName) {
+    return drivers.filter(function (individualDriver) {
+        return individualDriver.name === firstName
+    })
+}
+
+// driver.firstName = "Bobby"
+
+// {
+//     name: "Bobby"
+// }
+
+// const driver = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
