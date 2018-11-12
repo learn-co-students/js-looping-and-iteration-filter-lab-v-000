10477b97a5b8e3262f09b8f09f25d3cd455130a8
Anu Khambete <anukhambete@gmail.com>
2018-11-12 20:12:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+function findMatching (array, text) {
+let arr = array;
+let txt = text;
+  new_array = arr.filter(a => a.toLowerCase() === txt.toLowerCase());
+  return new_array;
+}
+
+function fuzzyMatch (array, text) {
+  let arr = array;
+  let txt = text;
+  new_array = arr.filter(a => a.slice(0,2) === txt.slice(0,2));
+  return new_array
+}
