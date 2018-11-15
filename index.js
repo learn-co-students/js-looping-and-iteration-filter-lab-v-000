a69a77e8dd17f509f1d9cb5fb64a3b295aeb1dc0
github username <github email address>
2018-11-15 16:36:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
 // Code your solution in this file
 function findMatching (collection, name) {
-  let newCollection = [];
-
-  for (let user of collection) {
-    if (user.toLowerCase() === name.toLowerCase()) {
-      newCollection.push(user);
-    }
-  }
-
-  return newCollection;
-};
+  return collection.filter(function (driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
 
 
 function fuzzyMatch (collection, substring) {
-  let newCollection = [];
-  let lowerSubstring = substring.toLowerCase();
-  let firstSubLetter = lowerSubstring[0];
+  let substringLength = substring.length;
 
-  for (let user of collection) {
-    let lowerUser = user.toLowerCase();
-    let firstLetter = lowerUser[0];
-
-    if (firstLetter === firstSubLetter) {
-      if (lowerUser.indexOf(lowerSubstring) !== -1) {
-        newCollection.push(user);
-      }
-    }
-  }
-  return newCollection;
+  return collection.filter(function (driver) {
+    return driver.toLowerCase().slice(0, substringLength) === substring.toLowerCase();
+  })
 };
 
 function matchName(collection, name) {
