b26a695a1152ec895352782a049fe25b97ea6b53
Robin Doirin <robindoirin@gmail.com>
2018-11-19 21:52:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file
+function findMatching(someArray, name) {
+  return someArray.filter(function (someName){
+    return someName.toLowerCase() == name.toLowerCase();
+  })
+}
+ function fuzzyMatch(someArray, str){
+  return someArray.filter(function(someStr) {
+    return someStr[0] == str[0];
+  })
+}
+ function matchName(someArray, str){
+  return someArray.filter(function(item){
+      return item.name == str;
+  })
+}
