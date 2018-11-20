a11404de3f140593e49cc306a9f36aaa9d81a7e4
jokerdelta <jokerdelta@yahoo.com>
2018-11-19 19:52:39 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file
+function findMatching (drivers, string) {
+  var result = drivers.filter(function(driver){
+    return driver.toUpperCase().includes(string.toUpperCase());
+  });
+
+  return result;
+}
+
+function fuzzyMatch (drivers,string){
+  var newCollection = [];
+    for (const driver of drivers){
+  if (driver.substring(0,2) === string.substring(0,2)) {
+    newCollection.push(driver);
+  }
+}
+
+return newCollection;
+}
+
+
+
+
+function matchName (drivers,string) {
+  var newCollection = [];
+    for (const driver of drivers){
+  if (driver.name === string) {
+    newCollection.push(driver);
+  }
+}
+
+return newCollection;
+
+}
+
