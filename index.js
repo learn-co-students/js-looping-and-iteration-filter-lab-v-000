e182aca421a516483300b5dab6e1d14ae49e9f50
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-11-26 13:27:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+
+function findMatching(drivers, name) {
+  return drivers.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, letters){
+  let lengthOfName = letters.length;
+  return drivers.filter(function (driverName){
+    return driverName.slice(0, lengthOfName) === letters;
+  });
+}
+
+function matchName(drivers, name){
+  return drivers.filter(function (driverName){
+    return driverName.name.toLowerCase() === name.toLowerCase();
+  });
+}
