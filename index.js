64ed473ce214ca2f0ae361635f79171201464d75
Will Schultz <will.j.schultz@gmail.com>
2018-11-30 04:50:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching(collection, name) {
+  return collection.filter(function (driverName){
+    return driverName.toLowerCase() === name.toLowerCase()
+  })
+}
+
+function fuzzyMatch (collection, name) {
+  let lengthOfName = name.length
+  return collection.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === name
+  })
+}
+
+function matchName (collection, name) {
+  return collection.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase()
+  })
+}
