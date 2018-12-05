f738aa5011b5aa4d37369552f579b0fd10cc08b8
bethurban <beth.urban@gmail.com>
2018-12-04 22:52:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file
+function findMatching(drivers, string) {
+  const names = drivers.filter(function(driver) { return driver.toLowerCase() == string.toLowerCase(); });
+  return names;
+}
+
+function fuzzyMatch(drivers, string) {
+  const names = drivers.filter(function(driver) { return driver[0] == string[0] && driver[1] == string[1]; });
+  return names;
+}
+
+function matchName(drivers, string) {
+  const names = drivers.filter(function(driver) { return driver.name == string; });
+  return names;
+}
