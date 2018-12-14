fcb022ce01234fe88d2c7d35330c04639b9d6202
btmccollum <btmcco8@gmail.com>
2018-12-14 16:24:33 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+// function findMatching(theArray, theString) {
+//     let newArr = theArray.filter(name => {if (name.toLowerCase() === theString.toLowerCase()) {return name;}});
+//     return newArr;
+// }
+function findMatching(arr, str) {
+    let newArr = arr.filter(function(name) { return str.toLowerCase() === name.toLowerCase();})
+    return newArr;
+}
+
+function fuzzyMatch(arr, letters) {
+    let newArr = arr.filter(function(name) { return name.slice(0,2) === letters;})
+    return newArr;
+}
+
+function matchName(arr, str) {
+    let newArr = arr.filter(function(object) { for (const key in object) { return object[key] === str; }})
+    return newArr;
+}
