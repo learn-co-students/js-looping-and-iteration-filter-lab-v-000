13c9fef0d48353c85c018175d3d1f97bd6e03542
Kachel Wittig <kjwittig@gmail.com>
2018-12-20 23:11:11 -0600

    Donezo

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+
+function findMatching(drivers, name) {
+return drivers.filter(function (driver) { return driver.toLowerCase() === name.toLowerCase() })
+}
+
+function fuzzyMatch(drivers, na) {
+  return drivers.filter(function (driver) { return driver.substring(0,2) === na.substring(0,2) })
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function (driver) { return driver.name === name })
+}
