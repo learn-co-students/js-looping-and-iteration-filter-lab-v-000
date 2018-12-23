a36b5c60ea221ac4e843c65076fc2b6ef190ef98
Jenny Kam <jkam17@gmail.com>
2018-12-23 00:15:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file
+//
+// function findMatching(arr, name){
+//   const n = []
+//   for (const x of arr){
+//     if (x.toLowerCase() === name.toLowerCase()){
+//       n.push(x)
+//     }
+//   }
+//   return n
+// }
+
+function findMatching(arr, name){
+  return arr.filter(
+    function(x){
+      return x.toLowerCase() === name.toLowerCase()
+    }
+  )
+}
+
+
+function fuzzyMatch(arr, name){
+  return arr.filter(
+    function(x){
+      return x.slice(0,name.length) === name
+    }
+  )
+}
+
+function matchName(arr, name){
+  const n = []
+  for (const x of arr){
+    if (x.name === name){
+      n.push(x)
+    }
+  }
+  return n
+}
