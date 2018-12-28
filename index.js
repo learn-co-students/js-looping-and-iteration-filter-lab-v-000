94ee4f9a98674192afafe51bbf11d617931075c3
sunyounghwang <sunyounghwang13@gmail.com>
2018-12-28 14:44:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file
+function findMatching(collection, name) {
+  return collection.filter(function (driver) { return driver.toLowerCase() === name.toLowerCase() });
+}
+
+function fuzzyMatch(collection, letters) {
+  return collection.filter(function (driver) { return driver.slice(0, letters.length) === letters });
+}
+
+function matchName(collection, name) {
+  return collection.filter(function (driverObj) { return driverObj.name === name });
+}
