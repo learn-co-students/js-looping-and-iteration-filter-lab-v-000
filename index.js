c39d768ce013af064fa8ccf87b20af1f5c31265d
Kayla Cross <kac288@georgetown.edu>
2018-12-31 14:11:01 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file
+
+function findMatching(drivers, name){
+  return drivers.filter(function(driverName){
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, snippet){
+  return drivers.filter(function(driverName){
+    let snippetLength = snippet.length;
+    return driverName.substring(0, snippetLength).toLowerCase() == snippet.toLowerCase();
+  });
+}
+
+function matchName(drivers, name){
+  return drivers.filter(function(driver){
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
