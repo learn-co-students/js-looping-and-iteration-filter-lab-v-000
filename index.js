8da54754c8d4882abba4186a178d936e78272079
github username <github email address>
2018-12-31 06:20:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+
+function findMatching(drivers, name) {
+  return drivers.filter(function(n) {return n.toLowerCase() === name.toLowerCase();});
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(function(n) {return n[0] === name[0];});
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(function(driver) {
+    return driver.name == name;});
+}
