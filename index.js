68343aa8301c2459310eff31f7b85e73804d48b4
ValerieHMoore <periwinkle17@outlook.com>
2019-01-07 16:33:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching(drivers, firstName) {
+    return drivers.filter(function (driverName) {
+        return driverName.toUpperCase() === firstName.toUpperCase();
+    });
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(function (driverName) {
+        return driverName.startsWith(`${letters}`);
+    });
+}
+
+function matchName(drivers, testName) {
+    return drivers.filter(function (driverName) {
+        return driverName.name === testName;
+    });
+}
