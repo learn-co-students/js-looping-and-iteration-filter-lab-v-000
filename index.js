94dea5bd223cb427b28a13937db42adaff041c17
nichia <nichia@gmail.com>
2019-01-09 14:22:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file
+function findMatching(collection, name) {
+  const newCollection = [];
+
+  for (const driver of collection) {
+    if (driver.toLowerCase() === name.toLowerCase()) {
+      newCollection.push(driver);
+    }
+  }
+  return newCollection;
+}
+
+function fuzzyMatch(collection, name) {
+  const newCollection = [];
+  const regex = new RegExp('^' + name, 'gi');
+  for (const driver of collection) {
+    if (driver.match(regex)) {
+      newCollection.push(driver);
+    }
+  }
+  return newCollection;
+}
+
+function matchName(collection, name) {
+  const newCollection = [];
+  for (const driver of collection) {
+    if (driver.name === name) {
+      newCollection.push(driver);
+    }
+  }
+  return newCollection;
+}
