712832519ed7e7324ec77b99e2bf6dfdff5a4000
nichia <nichia@gmail.com>
2019-01-09 14:29:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
 // Code your solution in this file
-function findMatching(collection, name) {
-  const newCollection = [];
-
-  for (const driver of collection) {
-    if (driver.toLowerCase() === name.toLowerCase()) {
-      newCollection.push(driver);
-    }
-  }
-  return newCollection;
+function findMatching (collection, name) {
+  return collection.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
 }
 
-function fuzzyMatch(collection, name) {
-  const newCollection = [];
-  const regex = new RegExp('^' + name, 'gi');
-  for (const driver of collection) {
-    if (driver.match(regex)) {
-      newCollection.push(driver);
-    }
-  }
-  return newCollection;
+function fuzzyMatch (collection, partialName) {
+  let lengthOfName = partialName.length;
+  return collection.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
 }
 
-function matchName(collection, name) {
-  const newCollection = [];
-  for (const driver of collection) {
-    if (driver.name === name) {
-      newCollection.push(driver);
-    }
-  }
-  return newCollection;
+function matchName (collection, name) {
+  return collection.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
 }
+
+// function findMatching(collection, name) {
+//   const newCollection = [];
+//
+//   for (const driver of collection) {
+//     if (driver.toLowerCase() === name.toLowerCase()) {
+//       newCollection.push(driver);
+//     }
+//   }
+//   return newCollection;
+// }
+//
+// function fuzzyMatch(collection, partialName) {
+//   const newCollection = [];
+//   const regex = new RegExp('^' + partialName, 'gi');
+//   for (const driver of collection) {
+//     if (driver.match(regex)) {
+//       newCollection.push(driver);
+//     }
+//   }
+//   return newCollection;
+// }
+//
+// function matchName(collection, name) {
+//   const newCollection = [];
+//   for (const driver of collection) {
+//     if (driver.name === name) {
+//       newCollection.push(driver);
+//     }
+//   }
+//   return newCollection;
+// }
