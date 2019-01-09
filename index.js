92515dfa2e7c59d8385208be25bfb80ac6833479
Greg Morgan <gregm9999@gmail.com>
2019-01-09 02:39:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file
+
+function findMatching(ar, name) {
+  return ar.filter(function(el)
+  {
+    return el.toLowerCase()===name.toLowerCase();
+  }
+)
+}
+
+
+function fuzzyMatch(ar, name) {
+  return ar.filter(function(el)
+  {
+    return el.toLowerCase().charAt(0)===name.toLowerCase().charAt(0);
+  }
+)
+}
+
+function matchName(ar, name) {
+  return ar.filter(function(el) {
+    return el.name===name;
+  })
+}
