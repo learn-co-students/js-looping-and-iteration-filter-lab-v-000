23736bc212fe30b2c5dca418a10ae4cfbcd8f496
Babs <36780653+51LV3RBR16HT@users.noreply.github.com>
2019-01-15 14:11:20 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching (drivers, names) {
+  return drivers.filter (function (driverName) {
+    return names.toLowerCase() === driverName.toLowerCase()
+  });
+}
+
+function fuzzyMatch (drivers, letters) {
+  return drivers.filter ( function (driverName) {
+    return driverName.slice (0, letters.length) === letters;
+  });
+}
+
+function matchName (drivers, name) {
+	return drivers.filter (function (driver) {
+		return driver.name === name
+	});
+}
