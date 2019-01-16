0f62ce0a1106a754bac9a4decde98068bf3520fd
Brenden Thornton <thorntonbrenden@gmail.com>
2019-01-16 14:42:23 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+
+function findMatching(list, name) {
+  return list.filter(function (driver) {
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch(list, partialName) {
+  let lengthOfName = partialName.length;
+
+  return list.filter(function (driver) {
+    return driver.slice(0, lengthOfName) === partialName;
+  });
+}
+
+function matchName(list, name) {
+  return list.filter(function (driver) {
+    return driver.name === name
+  });
+}
