400656e8c7a8bccd4ab643dad5346ad5e7d6a987
atxrenegade <harleighabel@gmail.com>
2019-01-16 02:28:41 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file
+function findMatching(driverArray, name) {
+	return driverArray.filter(function (driverName){
+		return driverName.toLowerCase() === name.toLowerCase()
+	});
+}
+
+function fuzzyMatch(driverArray, name) {
+	return driverArray.filter(function (matchingDriver){
+		return matchingDriver.slice(0,2) === name.slice(0,2)
+	});
+}
+
+function matchName(drivers, name) {
+	return drivers.filter(function (driverToMatch){
+		return driverToMatch.name.toLowerCase() === name.toLowerCase();
+	});
+}
