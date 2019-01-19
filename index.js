bcdaa08cabd466c04a54616b5f3c7d62a18a83e9
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-01-19 17:29:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching (collection, name) { 
+  return collection.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch (collection, nameChars) {
+  let nameLength = nameChars.length
+  return collection.filter(function (driverName) {
+    return driverName.slice(0, nameLength) === nameChars;
+  });
+}
+
+function matchName (collection, name) {
+  return collection.filter(function (driver) {
+   return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
