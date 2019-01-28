ca9c9bb1541adee23feeecbd5e764075118313a7
amitnyc83 <amit_nyc@live.com>
2019-01-28 09:18:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+function findMatching (drivers, string) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase() == string.toLowerCase();
+  });
+}
+
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function (driver){
+    return driver.toLowerCase().startsWith(string.toLowerCase());
+  });
+}
+
+function matchName (drivers, name) {
+  return drivers.filter(function (driver) {
+    return driver.name.toLowerCase() == name.toLowerCase();
+  });
+}
