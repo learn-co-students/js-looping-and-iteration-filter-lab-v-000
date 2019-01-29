b82a8c50de0a3df684105e6d9d0e96157ca3c857
freeWillee <will.ik.lee@gmail.com>
2019-01-29 12:11:30 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(collection, name) {
+    return collection.filter(function (driver) {return driver.toLowerCase() === name.toLowerCase(); });
+}
+
+function fuzzyMatch(collection, query) {
+    return collection.filter(function(driver) { return driver.slice(0, query.length) === query });
+}
+
+function matchName(collection, name) {
+    return collection.filter(function(obj) { return obj.name === name });
+}
