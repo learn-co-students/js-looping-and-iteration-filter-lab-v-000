c46b220f62dfba374694e0354ae88645dac150c8
Dani Lyn <dlyn1110@gmail.com>
2019-01-31 18:15:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file
+
+function findMatching (collection, name) {
+    return collection.filter(function (driverName) {
+        return driverName.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(collection, letter) {
+   return collection.filter(function (driverName) { return driverName.slice(0, letter.length) === letter;
+    });
+}
+
+function matchName(collection, name) {
+    return collection.filter(function (driverName) { return driverName.name === name;
+    })
+}
