5029c9d2f2f0ba0de5df8be510387831e8733cd6
github username <github email address>
2019-02-04 14:27:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 // Code your solution in this file
 function findMatching(array, string) {
-  newArray = array.filter(function (driver) { return driver.toUpperCase() == string.toUpperCase() });
-  return newArray;
+  return array.filter(function (driver) { return driver.toUpperCase() == string.toUpperCase() });
 }
 
 function fuzzyMatch(array, string) {
-  newArray = array.filter(function (driver) { return driver.toUpperCase()[0] + driver.toUpperCase()[1] == string.toUpperCase() });
-  return newArray;
+  return array.filter(function (driver) { return driver.toUpperCase()[0] + driver.toUpperCase()[1] == string.toUpperCase() });
 }
 
 function matchName(array, string) {
-  newArray = array.filter(function (driver) { return driver.name == string });
-  return newArray;
+  return array.filter(function (driver) { return driver.name == string });
 }
