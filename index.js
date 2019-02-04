ae368b87badace9d3ca39f4df2a0fd9858c69476
ConorHamilton19 <conhamil19@gmail.com>
2019-02-04 13:31:52 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file
+
+function findMatching(drivers, string) {
+    const matchingStrings = drivers.filter(function (name) {
+      return name.toUpperCase() == string.toUpperCase()})
+
+    return matchingStrings;
+}
+
+
+function fuzzyMatch(drivers, string) {
+  const fuzzyStrings = drivers.filter(function (name) {
+    return name.startsWith(string)});
+
+  return fuzzyStrings;
+}
+
+function matchName(drivers, string){
+    const matchedNames = drivers.filter(function(name) {
+      return name.name === string});
+
+    return matchedNames;
+    }
