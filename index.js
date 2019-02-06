f46ba3c25084dfafad0b2eebc7342d6a036126c3
Nikolai Pawlowski <nikolaip@gmail.com>
2019-02-06 18:08:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+function findMatching(drivers, arg) {
+  return drivers.filter(function(element) { return element.toLowerCase() === arg.toLowerCase(); });
+}
+
+
+function fuzzyMatch(drivers, arg) {
+  return drivers.filter(function(element) { return element.startsWith(arg) });
+}
+
+function matchName(drivers, arg) {
+  return drivers.filter(function(element) { return element.name === arg });
+}
