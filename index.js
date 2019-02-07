5a5f1b717ab4babcf47eb469e086b66cb6d5b184
ellefaraday <faraday.elle@gmail.com>
2019-02-07 13:40:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+function findMatching(array, name) {
+  return array.filter(function (list_name) {return name.toLowerCase() === list_name.toLowerCase()})
+}
+
+function fuzzyMatch(array, letters) {
+  return array.filter(
+    function (name) {return name.indexOf(letters) === 0}
+  )
+}
+
+function matchName(array, name) {
+  return array.filter(function (obj){return obj.name === name})
+}
