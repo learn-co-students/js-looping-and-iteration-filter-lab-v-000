ff3e78264997568259fcfeb033da31616d9bebe9
Pamela Frieze <pfrieze@gmail.com>
2019-02-07 20:42:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  return drivers.filter(function (driver) { 
+    return driver.toLowerCase() === string.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter(function (driver) { 
+    return driver[0,1] === string[0,1];
+  });
+}
+
+function matchName(drivers, string) {
+  return drivers.filter(function (driver) { 
+    return driver.name === string;
+  });
+}
