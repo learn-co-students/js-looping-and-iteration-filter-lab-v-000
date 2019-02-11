3e2aa402b6c5cb821b3eed76881b30a74e77698d
Christine Cua <cloudycua@gmail.com>
2019-02-11 16:26:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file
+function findMatching(list, arg) {
+  return list.filter(function (item) {
+    return item.toLowerCase() === arg.toLowerCase();
+  });
+}
+
+function fuzzyMatch(list, arg) {
+  return list.filter(function (item) {
+    return item.slice(0, arg.length).toLowerCase() === arg.toLowerCase();
+  });
+}
+
+function matchName(list, arg) {
+  return list.filter(function (item) {
+    return item.name.toLowerCase() === arg.toLowerCase();
+  });
+}
