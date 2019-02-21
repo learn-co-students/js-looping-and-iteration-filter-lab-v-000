448aa9e2cf9b7991131f578d693aea29af7f20dd
ryanwevans <ryanwevans@icloud.com>
2019-02-20 18:23:11 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file
+function findMatching(array, string) {
+  var string = new RegExp(string, 'gi');
+  return array.filter(function(name) { return name.match(string); });
+};
+
+function fuzzyMatch(array, string) {
+  var stringRegEx = new RegExp(string, 'g');
+  return array.filter(function(name) {
+    var newName = name.slice(0, string.length);
+    return newName.match(stringRegEx);
+  });
+};
+
+function matchName(array, string) {
+  return array.filter(function(driver) { return driver.name === string; });
+};
