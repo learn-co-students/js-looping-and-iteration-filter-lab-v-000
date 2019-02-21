e406625368d56707cac37cd87225f7407763f8db
Michael Dilley <mike.dilley@gmail.com>
2019-02-21 14:39:54 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+
+function findMatching(collection, string) {
+  return collection.filter( function(driverName) { return (driverName === string || driverName === string.toLowerCase()); });
+}
+
+function fuzzyMatch(collection, string) {
+  return collection.filter(function(driverName) { return driverName[0] === string[0]; } );
+}
+
+function matchName(collection, string) {
+  return collection.filter(function(driverObject) { return driverObject["name"] === string; } );
+}
