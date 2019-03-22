2c129b995a8952ccf8367ee044dd424a301bfacf
Melissa Real <melissa.real@hotmail.com>
2019-03-22 18:26:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file
+
+function findMatching(collection, name){
+  const result = collection.filter(n => n.toLowerCase()=== name.toLowerCase());
+  return result;
+}
+
+function fuzzyMatch(collection, initials){
+  const result = collection.filter(n => n.startsWith(initials));
+  return result;
+}
+
+function matchName(collection, name){
+  const newCollection = [];
+
+  for (const user of collection) {
+    if (user.name === name) {
+      newCollection.push(user);
+    }
+  }
+
+  return newCollection;
+}
