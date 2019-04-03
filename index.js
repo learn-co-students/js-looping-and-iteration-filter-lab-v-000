c058318081683dec08adf35fa744fe0a8296a627
rina71 <yurinas@gmail.com>
2019-04-03 01:43:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution in this file
+function findMatching(collection, driver){
+  return collection.filter(el => el.toLowerCase() === driver.toLowerCase());
+}
+
+function fuzzyMatch(collection, letter){
+   return collection.filter(el => el.indexOf(letter) === 0);
+}
+
+function matchName(collection, name){
+  return collection.filter(el => el.name === name);
+
+}
