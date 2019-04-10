a8c98d6f662f94a136bc6c59ba2e872c59030e64
Rachel Wyatt <rw126@evansville.edu>
2019-04-10 19:46:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file
+function findMatching(drivers, string) {
+  let findByName = drivers.filter(function (name) { return name == string || name == string.toLowerCase(); });
+  return findByName
+}
+
+function fuzzyMatch(drivers, string) {
+  let findByFirstInitial = drivers.filter(name => name.startsWith(string));
+  return findByFirstInitial
+}
+
+function matchName(drivers, string) {
+  let nameMatch = drivers.filter(function (driver) { return driver.name == string; });
+  return nameMatch
+}
