ff0cd48d841fe35e12d75eb9eba13d053608311e
George  Pianka <gpianka1@gmail.com>
2019-04-18 07:36:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(drivers, selection) {
+  return drivers.filter(function(driver) { return driver.toLowerCase() === selection.toLowerCase() });
+}
+
+function fuzzyMatch(drivers, selection) {
+  return drivers.filter(function(driver) { return driver.startsWith(selection) });
+}
+
+function matchName(drivers, selection) {
+  return drivers.filter(function(driver) {return driver.name === selection });
+}
