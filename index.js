76ce5ee3fd40396fe69c8a4ffcbbb320f95ebb21
George  Pianka <gpianka1@gmail.com>
2019-04-18 07:35:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ function findMatching(drivers, selection) {
 function fuzzyMatch(drivers, selection) {
   return drivers.filter(function(driver) { return driver.startsWith(selection) });
 }
+
+function matchName(drivers, who) {
+  return drivers.filter(function(driver) {return driver.name === who });
