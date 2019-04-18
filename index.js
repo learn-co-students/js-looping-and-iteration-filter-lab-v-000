3e6a2e799d5093f04a8fa0c6887911ff5967e876
George  Pianka <gpianka1@gmail.com>
2019-04-18 07:34:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function findMatching(drivers, selection) {
   return drivers.filter(function(driver) { return driver.toLowerCase() === selection.toLowerCase() });
 }
+
+function fuzzyMatch(drivers, selection) {
+  return drivers.filter(function(driver) { return driver.startsWith(selection) });
+}
