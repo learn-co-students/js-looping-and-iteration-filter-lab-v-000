192d0871f497f2cd8f6475f507b3841a57dc9bcb
Kellye Greene <kellyecodes@gmail.com>
2019-04-19 17:52:22 -0400

    Updates test suite

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,3 @@
-const expect = chai.expect;
-
 describe('index.js', function () {
   describe('findMatching()', function () {
     it('returns all drivers that match the passed in name', function () {
