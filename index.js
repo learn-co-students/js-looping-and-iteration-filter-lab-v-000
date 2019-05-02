65632b8391607d42fcd121f91435de914059214d
stazman <cadistasio@gmail.com>
2019-05-02 10:18:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 function findMatching (collection, driver) {
-    return collection.filter(function (driverName) { 
-        return driverName.toLowerCase() === driver.toLowerCase();
-    });
+    return collection.filter( driverName => driverName.toLowerCase() === driver.toLowerCase());
 } 
     
 function fuzzyMatch (collection, driver) {
-    return collection.filter(function (driverName) {
-        return driverName.slice(0, 1) === driver.slice(0, 1); 
-    });
+    return collection.filter( driverName => driverName.slice(0, 1) === driver.slice(0, 1));
 }
 
 function matchName (collection, driver) {
-    return collection.filter(function (driverName) {
-        return driverName.name.toLowerCase() === driver.toLowerCase(); 
-    });
+    return collection.filter( driverName => driverName.name.toLowerCase() === driver.toLowerCase());
 }
