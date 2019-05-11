32e80abf010fadc5d27c978a7a3f2e35eb2cb742
wendycalderon@gmail.com <wendycalderon@gmail.com>
2019-05-11 11:17:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+    let newDrivers = drivers.filter(function (driver) {return driver.toLowerCase() === name.toLowerCase(); });
+    return newDrivers;
+}
+
+function fuzzyMatch(drivers, name) {
+    let newDrivers = drivers.filter(function (driver) {return driver.slice(0, name.length) === name; });
+    return newDrivers;
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function(driver) {return driver.name.toLowerCase() === name.toLowerCase()} );
+}
