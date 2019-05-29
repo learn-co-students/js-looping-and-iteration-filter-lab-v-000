39955e5a2e3ca25c7726118f60e2e065beb78739
lpzraf <lpz.raf@gmail.com>
2019-05-28 22:31:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+
+function findMatching(drivers, name) {
+  let newDrivers = drivers.filter(function(driver) {
+    return driver.toLowerCase() === name.toLowerCase(); });
+    return newDrivers;
+}
+
+function fuzzyMatch(drivers, index, name) {
+  let filteredDrivers = drivers.filter(function(driver) {
+    return driver.charAt(index) === name;
+   });
+    return filteredDrivers;
+}
