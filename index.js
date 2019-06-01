ef299a1c8e1bae50546bceaf924adcf6f087d75e
sarawall1187 <sarawall1187@gmail.com>
2019-06-01 09:30:12 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file
+function findMatching(arr, string) {
+   
+   return arr.filter(function(string) {return arr.ignoreCase() === string.ignoreCase();});
+}
+
+function fuzzyMatch(arr, string) {
+    return arr.filter(function(el) {
+        return el.ignoreCase().indexOf(string.ignoreCase()) !== -1;
+    })
+  }
+  
+ function matchName(arr, string) {
+
+ }
