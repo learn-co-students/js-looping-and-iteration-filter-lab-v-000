ed9adc966bb456c5c698eac080bf3911bf5aebcc
Grace Choi <oiograceoio@gmail.com>
2019-06-03 14:34:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file
+function findMatching(collection, name) {
+	return collection.filter(function(driver) { 
+		return driver.toLowerCase() === name.toLowerCase(); 
+	});
+}
+
+function fuzzyMatch(collection, letters) {
+	let size = letters.length;
+
+	return collection.filter(function(name) {
+		return name.slice(0, size) === letters
+	});
+}
+
+function matchName(collection, name) {
+	return collection.filter(function(person) {
+		return person.name === name;
+	})
+}
