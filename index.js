317941441d92ce5fdc406a6fabcf80a259f1523f
Ashley <ashleywong678@gmail.com>
2019-06-04 08:57:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(drivers, name){
-  return drivers.filter(function (driver) {return driver == name});
+  return drivers.filter(driver => driver == name );
 }
