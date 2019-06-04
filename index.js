3fd6096e47baff73979eaec10e5bf618bedf2d40
Ashley <ashleywong678@gmail.com>
2019-06-04 09:00:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file
 function findMatching(drivers, name){
-  return drivers.filter(driver => driver == name );
+  return drivers.filter(function (driver) {return driver.toLowerCase() == name});
 }
