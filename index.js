d655acdeee86c5fcefedf48923e9f26d64607da2
Brian Arango <brianvictus@gmail.com>
2019-06-19 01:20:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function filter (collection, attribute, value) {
+  for (const user of collection) {
+    if (user[attribute] === value) {
+      console.log(user.firstName);
+    }
+  }
+}
