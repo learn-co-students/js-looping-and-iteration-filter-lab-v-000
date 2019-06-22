153ff0311e5b9fb258ee9374cee7341869a772ed
Dan Hintz <dhintz@live.com>
2019-06-22 21:09:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Code your solution in this file
 function findMatching(arr, string) {
-  const filteredDrivers = arr.filter(function (string) {return arr.})
+  const filteredDrivers = arr.filter(function (element) {return element.toLowerCase() === string.toLowerCase()});
+  return filteredDrivers;
 }
 
-function fuzzyMatch() {
-  
+function fuzzyMatch(arr, string) {
+  return arr.filter(function (el) {return el.toLowerCase().startsWith(string.toLowerCase());});
 }
 
-function matchName() {
-  
+function matchName(arr, string) {
+  return arr.filter(function (driver) {return driver.name === string;});
 }
