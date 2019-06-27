219ac92b8bdec01f13333c66f6e635951374a3ce
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-06-26 22:26:38 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+
+function findMatching(drivers, query) {
+    let driverMatch = [];
+    driverMatch = drivers.filter(function (driver) { return driver.toLowerCase() === query.toLowerCase()});
+    return driverMatch;
+}
+
+function fuzzyMatch(drivers, letters) {
+  let stringLength = letters.length;
+  let driverMatch = drivers.filter(function (driver) {return driver.slice(0, stringLength) == letters });
+  return driverMatch;
+
+}
+
+function matchName(drivers, query) {
+  let nameMatch = drivers.filter(function (driver) {return driver.name == query});
+  return nameMatch;
+}
