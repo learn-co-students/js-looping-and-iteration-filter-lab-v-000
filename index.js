d2ff5f38c1026365dc4920921bd306ba4d0a86c4
github username <github email address>
2019-06-29 17:48:05 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file
+
+
+function findMatching (drivers, name) {
+  return drivers.filter(function (driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+  });
+}
+
+function fuzzyMatch (drivers, name) {
+  return drivers.filter(function (driverName) {
+    return driverName[0] === name[0];
+  });
+}
+
+function matchName (drivers, name) {
+  return drivers.filter(function (driverName) {
+    return driverName.name.toLowerCase() === name.toLowerCase();
+  });
+}
+
+
+
+//Write matchName - This function takes an array of drivers and a string as
+//arguments. In this function, each element of the drivers array is a JavaScript
+// object that has a property of name. The function should return each element
+//whose name property matches the provided string argument.
+
+
+//Write fuzzyMatch - This function takes an array of drivers and a string as
+// arguments for querying the array, and returns all drivers whose names begin
+//with the provided letters.
+
+
+//Write findMatching- This function takes an array of drivers and a string
+//as arguments, and returns the matching list of drivers. The function should be case insensitive.
