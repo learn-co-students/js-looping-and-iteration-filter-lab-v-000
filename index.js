d450741027200f00a59529235a99d22c754eccfe
leothebeagle <a.kosber@gmail.com>
2019-08-28 16:38:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching(collection, driverName) {
+    return collection.filter(function(collectionItem) { return collectionItem.toLowerCase() === driverName.toLowerCase(); });
+}
+
+function fuzzyMatch(collection, stringToMatch) {
+    return collection.filter(function(collectionItem) { return collectionItem.slice(0, stringToMatch.length) === stringToMatch});
+}
+
+function matchName(collection, nameToMatch) {
+    return collection.filter(function(collectionItem) { return collectionItem.name === nameToMatch; });
+}
