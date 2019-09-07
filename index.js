96ccb8a179b350425a5fa20a86dff16d63c33d72
Amanda Murphy <helloamandamurphy@gmail.com>
2019-09-07 14:17:50 -0400

    Done.

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ function findMatching (drivers, name) {
   });
 }
 
-function fuzzyMatch (drivers, partialName) {
-  return drivers.filter(function (partialMatch) {
-    return partialMatch.slice(0, (partialName.length)) === partialName;
+function fuzzyMatch(drivers, partialName) {
+  return drivers.filter(function (partial) {
+    return partial.slice(0,(partialName.length)) === partialName;
   });
 }
 
 function matchName(drivers, name) {
-  return drivers.filter(function (driver) {
-    return driver.name.toLowerCase() === name.toLowerCase();
+  return drivers.filter(function (matching) {
+    return matching.name === name;
   });
 }
