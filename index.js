91193724e4d9d5559eba109667fd59bc55dfb9d3
Nate Dietz <nsdietz@gmail.com>
2019-09-06 23:13:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+function findMatching (drivers, name) {
+	return drivers.filter( function (userName) { return userName.toLowerCase().indexOf(name.toLowerCase()) !== -1; })
+}
+
+function fuzzyMatch (drivers, char) {
+	return drivers.filter( function (val) { return val.startsWith(char); })
+}
+
+function matchName(drivers, name) {
+	return drivers.filter( function (driver) { return driver.name === name; })
+}
