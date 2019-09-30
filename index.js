43346af58244f0719fb6c4f8bfabb30d13aae040
Rebecca Tong <rtong@buffalo.edu>
2019-09-30 17:56:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+
+
+function findMatching(list,driverName){
+return list.filter(function(name){ return name.toLowerCase() === driverName.toLowerCase()})
+}
+
+function fuzzyMatch(drivers,string){
+  const line = string.length
+  return drivers.filter(function(name){ return name.slice(0,line) === string})
+}
+
+function matchName(drivers,string){
+  return drivers.filter(function(element){
+    return element.name === string
+  })
+
+}
