37afec02866e206d487dc2e3e2d3421404e3580f
RonSala <ron.sala@gmail.com>
2020-03-01 20:31:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file
+function findMatching(array, string) {
+  let result = [];
+  for (const e of array) {
+    let lowerCaseName = e.toLowerCase();
+    let lowerCaseString = string.toLowerCase();
+    if (lowerCaseName === lowerCaseString) {
+      result.push(e);
+    }
+  }
+  return result;
+ }
+
+ function fuzzyMatch(array, string) {
+  let re = new RegExp(string);
+  // let result = array.filter(function(name) {
+  //   return name.match(re);
+  // });
+  // return result;
+ }
+
+ function matchName(array, string) {
+
+ }
