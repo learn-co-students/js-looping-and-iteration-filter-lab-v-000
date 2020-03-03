8c54122a554bf1d12b140ff47f98a21a5105df9d
RonSala <ron.sala@gmail.com>
2020-03-03 14:45:18 -0500

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function fuzzyMatch(ary, str) {
   return ary.filter(e => e.substring(0, 2).match(str));
 }
 
-function matchName(array, string) {
-
+function matchName(ary, str) {
+  return ary.filter(e => e.name.match(str));
 }
